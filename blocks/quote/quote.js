ab57558644c27884485d8657b2e69fd579ba6f41
prakharpathak <prakhar.pathak@nagarro.com>
2024-05-22 17:24:29 +0530

    adding quote helix block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
+  console.log("inside quote");
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
