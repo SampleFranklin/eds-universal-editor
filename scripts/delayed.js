3232f3f3af322e61a6a1eacbc3f5f83a60231ed6
karrireddy <karri.reddy@nagarro.com>
2024-05-28 17:04:56 +0530

    comment metadata

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,20 +6,20 @@ sampleRUM('cwv');
 
 // add more delayed functionality here
 
-function loadMetadata() {
-    fetch('/metadata.json').then( res => {
-        if(res.ok) {
-            res.json().then(data => {
-                data.data.forEach(item => {
-                    const meta = document.createElement('meta');
-                    Object.keys(item).forEach(key => {
-                        meta.setAttribute(key, item[key]);
-                    });
-                    document.getElementsByTagName('head')[0].appendChild(meta);
-                });
-            });
-        }
-    });
-}
+// function loadMetadata() {
+//     fetch('/metadata.json').then( res => {
+//         if(res.ok) {
+//             res.json().then(data => {
+//                 data.data.forEach(item => {
+//                     const meta = document.createElement('meta');
+//                     Object.keys(item).forEach(key => {
+//                         meta.setAttribute(key, item[key]);
+//                     });
+//                     document.getElementsByTagName('head')[0].appendChild(meta);
+//                 });
+//             });
+//         }
+//     });
+// }
 
-loadMetadata()
+// loadMetadata()
