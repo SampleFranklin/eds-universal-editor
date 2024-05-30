18e89ef065efb3e5d297e6736e5ab30bf6465da5
prakharpathak <prakhar.pathak@nagarro.com>
2024-05-30 17:18:36 +0530

    added chatbot snippet

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,8 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+var chatbotScript = `<script src="//unity-plugin.iamdave.ai/conversation-plugin/build/2.0/dave-libs.js" id="dave-settings" data-environment="staging" data-asserts="//unity-plugin.iamdave.ai/conversation-plugin" data-loadfunction='onload' data-component="babylon-avatar-conversation,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.js,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.css"></script>`
+
+const chatbotDiv = document.createElement('div');
+chatbotDiv.innerHTML(chatbotScript);
+document.getElementsByTagName('body')[0].appendChild(chatbotDiv.firstChild);
