9d0cf4b89583e017b860ca782f58de74857a83a2
prakharpathak <prakhar.pathak@nagarro.com>
2024-05-30 17:55:46 +0530

    chatbot snippet

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,13 +5,19 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-var chatbotScript = `<script src="https://unity-plugin.iamdave.ai/conversation-plugin/build/2.0/dave-libs.js" id="dave-settings" data-environment="staging" data-asserts="//unity-plugin.iamdave.ai/conversation-plugin" data-loadfunction='onload' data-component="babylon-avatar-conversation,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.js,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.css"></script>`
+// var chatbotScript = `<script src="//unity-plugin.iamdave.ai/conversation-plugin/build/2.0/dave-libs.js" id="dave-settings" data-environment="staging" data-asserts="//unity-plugin.iamdave.ai/conversation-plugin" data-loadfunction='onload' data-component="babylon-avatar-conversation,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.js,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.css"></script>`
 
-const chatbotDiv = document.createElement('div');
-chatbotDiv.innerHTML = chatbotScript;
-chatbotDiv.firstChild.type = "text/javascript";
-chatbotDiv.firstChild.onload = function(){
-    console.log("Chatbot loaded");
-}
-document.getElementsByTagName('body')[0].appendChild(chatbotDiv.firstChild);
+// const chatbotDiv = document.createElement('div');
+// chatbotDiv.innerHTML = chatbotScript;
+
+var chatScript = document.createElement('script');
+chatScript.src = "//unity-plugin.iamdave.ai/conversation-plugin/build/2.0/dave-libs.js";
+chatScript.id = "dave-settings";
+chatScript.setAttribute("data-environment","staging");
+chatScript.setAttribute("data-loadfunction","onload");
+chatScript.setAttribute("data-component","babylon-avatar-conversation,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.js,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.css")
+chatScript.setAttribute("data-asserts","//unity-plugin.iamdave.ai/conversation-plugin");
+chatScript.type = "text/javascript";
+
+document.getElementsByTagName('body')[0].appendChild(chatScript);
 
