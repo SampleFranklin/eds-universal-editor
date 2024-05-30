5580e16437c7c9d51236f2ce2aad4266a849d2bd
prakharpathak <prakhar.pathak@nagarro.com>
2024-05-30 17:21:10 +0530

    chatbot snippet

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -8,5 +8,5 @@ sampleRUM('cwv');
 var chatbotScript = `<script src="//unity-plugin.iamdave.ai/conversation-plugin/build/2.0/dave-libs.js" id="dave-settings" data-environment="staging" data-asserts="//unity-plugin.iamdave.ai/conversation-plugin" data-loadfunction='onload' data-component="babylon-avatar-conversation,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.js,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.css"></script>`
 
 const chatbotDiv = document.createElement('div');
-chatbotDiv.innerHTML(chatbotScript);
+chatbotDiv.innerHTML = chatbotScript;
 document.getElementsByTagName('body')[0].appendChild(chatbotDiv.firstChild);
