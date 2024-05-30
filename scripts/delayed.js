3bd8c4ddb34ba11f2917e63a98dcf9f4da2814f0
prakharpathak <prakhar.pathak@nagarro.com>
2024-05-30 18:37:36 +0530

    added chatbot snippet

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -14,17 +14,17 @@ const chatDiv = document.createElement('div');
 chatDiv.innerHTML = `<div class="dave-main-container" id="dave-settings" data-dave-components="chatbot-speech-avatar-wjq-help,maruti_accessories/2.0/arena.js" data-app-version="2.0" data-dave-version="9.1"></div><div class="dave-help-container" id="dave-help" data-timeout=15000></div>`
 
 var chatScript = document.createElement('script');
-// chatScript.src = "//unity-plugin.iamdave.ai/conversation-plugin/build/2.0/dave-libs.js";
-chatScript.src = "//chatbot-plugin.iamdave.ai/assets/js/9.1/library-loader.js";
-// chatScript.id = "dave-settings";
-// chatScript.setAttribute("data-environment","staging");
-// chatScript.setAttribute("data-loadfunction","onload");
-// chatScript.setAttribute("data-component","babylon-avatar-conversation,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.js,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.css")
-// chatScript.setAttribute("data-asserts","//unity-plugin.iamdave.ai/conversation-plugin");
+chatScript.src = "//unity-plugin.iamdave.ai/conversation-plugin/build/2.0/dave-libs.js";
+// chatScript.src = "//chatbot-plugin.iamdave.ai/assets/js/9.1/library-loader.js";
+chatScript.id = "dave-settings";
+chatScript.setAttribute("data-environment","staging");
+chatScript.setAttribute("data-loadfunction","onload");
+chatScript.setAttribute("data-component","babylon-avatar-conversation,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.js,//d3nxn5bl526zn7.cloudfront.net/maruti_brands/new_swift/3.0/web.css")
+chatScript.setAttribute("data-asserts","//unity-plugin.iamdave.ai/conversation-plugin");
 chatScript.type = "text/javascript";
 
 const bodyTag = document.getElementsByTagName('body')[0];
-bodyTag.appendChild(chatDiv);
+// bodyTag.appendChild(chatDiv);
 bodyTag.appendChild(chatScript);
 // document.getElementsByTagName('body')[0].appendChild(chatScript);
 
