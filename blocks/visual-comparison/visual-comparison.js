5d6466f28ff8fac5db9b194f707725537d2627ca
riyaguptacb <riya.gupta@crowdbotics.com>
2024-06-07 15:13:40 +0530

    DOM creation for visual comparison

--- blocks/visual-comparison/visual-comparison.js
+++ blocks/visual-comparison/visual-comparison.js
@@ -1,8 +1,64 @@
 export default function decorate(block) {
     console.log("inside visualComparison");
-    console.log(block);
+    console.log("riya block check", block);
 
+    const links =  block.querySelectorAll(':scope > div > div > p > a');
+    const outerDiv = document.createElement('div');
+    outerDiv.classList.add('outer-div');
+    outerDiv.innerHTML="<p>hey outer div</p>"
 
-    const values = document.querySelector(':scope > div > div > p');
-    console.log("vcompare:"+values);
+    const videos = [];
+
+    links.forEach((link)=>{
+        const url = link.attributes['href'].nodeValue;
+        const innerDiv = document.createElement('div');
+        innerDiv.classList.add('inner-div');
+
+        innerDiv.innerHTML = "<p>hey inner div</p>"
+
+        const video = document.createElement('video');
+        video.src = url;
+        video.loop = true;
+        video.autoplay = true;
+        video.setAttribute("aria-label", "altText Video");
+
+        innerDiv.appendChild(video);
+        outerDiv.appendChild(innerDiv);
+
+        videos.push(video);
+    })
+    const [video1, video2] = [videos[0], videos[1]]
+    function syncPlay(){
+        if (video1.paused !== video2.paused) {
+          if (video1.paused) {
+            video2.pause();
+          } else {
+            video2.play();
+          }
+        }
+      };
+    function syncPause(){
+        if (video1.paused !== video2.paused) {
+          if (video1.paused) {
+            video2.pause();
+          } else {
+            video2.play();
+          }
+        }
+      };
+    function syncTimeUpdate() {
+        if (Math.abs(video1.currentTime - video2.currentTime) > 0.1) {
+          video2.currentTime = video1.currentTime;
+        }
+      };
+
+    video1.addEventListener("play", syncPlay);
+    video1.addEventListener("pause", syncPause);
+    video1.addEventListener("timeupdate", syncTimeUpdate);
+
+    block.textContent = '';
+    block.append(outerDiv);
+
+    // const values = document.querySelector(':scope > div > div > p');
+    // console.log("vcompare:"+values);
 }
