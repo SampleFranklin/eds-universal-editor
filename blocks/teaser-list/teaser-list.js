2943b3b151b5c2e39e6229594bf44e23800d9d45
karrireddy <karri.reddy@nagarro.com>
2024-06-11 15:18:49 +0530

    update teaser list

--- blocks/teaser-list/teaser-list.js
+++ blocks/teaser-list/teaser-list.js
@@ -1,11 +1,12 @@
+import { moveInstrumentation } from "../../scripts/scripts";
+
 export default function decorate(block) {
     console.log(block.cloneNode(true));
+
     const commonTitle = block.querySelector('.teaser-list > div:first-child > div > p').innerText;
     let newHtml = `
     <div>
-        <div>
-            <h1>${commonTitle}</h1>
-        </div>
+        <h1>${commonTitle}</h1>
     </div>
     <div style="display: flex;">`;
 
@@ -21,7 +22,7 @@ export default function decorate(block) {
         const cta1 = ctaElements[0] ? ctaElements[0].outerHTML : '';
         const cta2 = ctaElements[1] ? ctaElements[1].outerHTML : '';
 
-        newHtml += `
+        card.innerHtml = `
                 <div style="border: 1px solid #ccc; margin: 10px; padding: 10px;">
                     <img src="${imgSrc}" alt="" style="max-width: 100%;">
                     <p>${preTitle}</p>
@@ -30,7 +31,10 @@ export default function decorate(block) {
                     <div>${cta1}</div>
                     <div>${cta2}</div>
                 </div>
-            `;
+        `;
+        const teaserDiv = document.createElement('div');
+        teaserDiv.append(card);
+        newHtml += `${teaserDiv.innerHTML}`;
     });
 
     newHtml += `</div>`;
