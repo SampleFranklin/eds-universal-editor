e60d5b03ec4635758778bb36337fb3de4e0749ce
prakharpathak <prakhar.pathak@nagarro.com>
2024-06-11 12:56:05 +0530

    link grid

--- blocks/link-grid/link-grid.js
+++ blocks/link-grid/link-grid.js
@@ -1,29 +1,22 @@
 export default function decorate(block) {
-    const gridContainer = document.createElement('div');
+    var gridContainer = document.createElement('div');
     gridContainer.className = 'link-container-section';
-    const columns = []
-    for (let i = 0; i < block.children.length; i++) {
-        const item = block.children.item(i);
-        const el = item.querySelector('p');
-        if (el) {
-            columns.push(item);
-        }
-    }
-    columns.forEach(function (el) {
-        const columnDiv = document.createElement('div');
+    var tabHeadings = block.querySelectorAll('.link-grid h3');
+    tabHeadings.forEach(function (heading, index) {
+        var columnDiv = document.createElement('div');
         columnDiv.className = 'link-grid-column';
 
-        const buttonContainer = document.createElement('p');
+        var buttonContainer = document.createElement('p');
         buttonContainer.className = 'button-container';
 
-        const tabDiv = el;
+        var tabDiv = heading.parentNode;
 
-        const links = tabDiv.querySelectorAll('p.button-container a');
+        var links = tabDiv.querySelectorAll('p.button-container a');
         links.forEach(function (link) {
-            const linkText = link.textContent;
-            const href = link.getAttribute('href');
+            var linkText = link.textContent;
+            var href = link.getAttribute('href');
 
-            const a = document.createElement('a');
+            var a = document.createElement('a');
             a.textContent = linkText;
             a.href = href;
 
@@ -36,18 +29,12 @@ export default function decorate(block) {
             buttonContainer.appendChild(a);
         });
 
-        if(el.querySelector('h3')) {
-            columnDiv.appendChild(el.querySelector('h3'));
-        } else {
-            columnDiv.classList.add('no-heading-column');
-        }
+        columnDiv.appendChild(heading.cloneNode(true));
         columnDiv.appendChild(buttonContainer);
         gridContainer.appendChild(columnDiv);
     });
-
     block.innerHTML = "";
     block.append(gridContainer);
-    console.log(block);
 }
 
 function isInternalLink(href) {
