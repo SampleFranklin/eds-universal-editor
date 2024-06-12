5d17c107de66ecc67aedfa62ec9c0e39dcdf36a1
karrireddy <karri.reddy@nagarro.com>
2024-06-12 19:24:16 +0530

    update teaser list

--- blocks/teaser-list/teaser-list.js
+++ blocks/teaser-list/teaser-list.js
@@ -1,22 +1,22 @@
 export default function decorate(block) {
     const commonTitle = block.querySelector('.teaser-list > div:first-child > div > p').innerText;
     const teasers = Array.from(block.querySelectorAll('.teaser-list > div:not(:first-child)')).map(card => {
-        const imgSrc = card.querySelector('img').src;
-        const preTitle = card.querySelector('div:nth-of-type(2) > p').innerText;
-        const title = card.querySelector('div:nth-of-type(3) > p').innerText;
-        const description = Array.from(card.querySelectorAll('div:nth-of-type(4) p:not(.button-container)')).map(p => p.innerText).join(' ');
+        const imgSrc = card.querySelector('img')?.src;
+        const altText = card.querySelector('div:nth-of-type(2) > p')?.innerText;
+        const preTitle = card.querySelector('div:nth-of-type(3) > p')?.innerText;
+        const title = card.querySelector('div:nth-of-type(4) > p')?.innerText;
+        const description = Array.from(card.querySelectorAll('div:nth-of-type(5) p:not(.button-container)')).map(p => p.innerText).join(' ');
 
-        const ctaElements = card.querySelectorAll('div:nth-of-type(4) p.button-container a');
-        const cta1 = ctaElements[0] ? ctaElements[0].outerHTML : '';
-        const cta2 = ctaElements[1] ? ctaElements[1].outerHTML : '';
+        const ctaElements = card.querySelectorAll('div:nth-of-type(5) p.button-container a');
+        const ctas = Array.from(ctaElements).map(cta => cta.outerHTML);
+
         card.innerHTML = `
             <div style="border: 1px solid #ccc; margin: 10px; padding: 10px;">
-                <img src="${imgSrc}" alt="" style="max-width: 100%;">
+                <img src="${imgSrc}" alt=${altText || title} style="max-width: 100%;">
                 <p>${preTitle}</p>
                 <h2>${title}</h2>
                 <p>${description}</p>
-                <div>${cta1}</div>
-                <div>${cta2}</div>
+                ${ctas.map(cta => `<div>${cta}</div>`).join('')}
             </div>
         `;
         return card.outerHTML;
