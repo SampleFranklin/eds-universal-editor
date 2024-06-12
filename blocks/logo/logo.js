060751942ded184d915a429284d586512b497855
prakharpathak <prakhar.pathak@nagarro.com>
2024-06-12 19:22:17 +0530

    block alt-text

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -1,8 +1,12 @@
 export default function decorate(block) {
     var link =  block.querySelector(':scope > div > div > p > a');
+    var altText =  block.querySelector(':scope > div > div > p');
     var imgTag =  block.querySelector(':scope > div > div > picture');
+    var image =  block.querySelector(':scope > div > div > picture > img');
     var pTag = document.createElement('p');
     var anchorTag = document.createElement('a');
+
+    image.setAttribute("alt",altText.innerHTML);
 
     anchorTag.href = link;
     anchorTag.appendChild(imgTag);
@@ -10,5 +14,7 @@ export default function decorate(block) {
     pTag.appendChild(anchorTag);
     block.innerHTML = "";
     block.append(pTag);
+
+    console.log(block);
 }
 
