63ffd1613acf625e396042a40ae786b2baab9cef
prakharpathak <prakhar.pathak@nagarro.com>
2024-06-12 18:30:16 +0530

    logo block changes

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -3,13 +3,12 @@ export default function decorate(block) {
     var imgTag =  block.querySelector(':scope > div > div > picture');
     var pTag = document.createElement('p');
     var anchorTag = document.createElement('a');
-    
+
     anchorTag.href = link;
     anchorTag.appendChild(imgTag);
 
     pTag.appendChild(anchorTag);
-
-    var navBlockDiv = block.parentNode;
-    navBlockDiv.replaceWith(pTag);
+    block.innerHTML = "";
+    block.append(pTag);
 }
 
