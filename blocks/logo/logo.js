a3163b72f1b0ff8f7a483c37e9ef4eb98cad2537
brijendrayadav <brijendra.yadav@nagarro.com>
2024-06-13 11:51:50 +0530

    css changes

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -1,16 +1,34 @@
+// export default function decorate(block) {
+//     var link =  block.querySelector(':scope > div > div > p > a');
+//     var imgTag =  block.querySelector(':scope > div > div > picture');
+//     var pTag = document.createElement('p');
+//     var anchorTag = document.createElement('a');
+    
+//     anchorTag.href = link;
+//     anchorTag.target='_self';
+//     anchorTag.appendChild(imgTag);
+
+//     pTag.appendChild(anchorTag);
+//     pTag.className='logo';
+
+//     var navBlockDiv = block.parentNode;
+//     navBlockDiv.replaceWith(pTag);
+// }
 export default function decorate(block) {
     var link =  block.querySelector(':scope > div > div > p > a');
+    var altText =  block.querySelector(':scope > div > div > p');
     var imgTag =  block.querySelector(':scope > div > div > picture');
+    var image =  block.querySelector(':scope > div > div > picture > img');
     var pTag = document.createElement('p');
     var anchorTag = document.createElement('a');
-    
+
+    image.setAttribute("alt",altText.innerHTML);
+
     anchorTag.href = link;
-    anchorTag.target='_self';
     anchorTag.appendChild(imgTag);
+    anchorTag.classList.add('logo');
 
     pTag.appendChild(anchorTag);
-    pTag.className='logo';
-
-    var navBlockDiv = block.parentNode;
-    navBlockDiv.replaceWith(pTag);
+    block.innerHTML = "";
+    block.append(pTag);
 }
