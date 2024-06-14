15a33f44ca365617eb29a8a8b7296d82fbced705
karrireddy <karri.reddy@nagarro.com>
2024-06-14 12:44:46 +0530

    remove immersive teaser

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,52 +1,6 @@
 import { getContent } from '../../scripts/utils.js';
 
 export default function decorate(block) {
-    block.innerHTML = getTeaserHTML(block);
-//   const image = block.querySelector('picture');
-
-//   const altText = block.querySelector('div:nth-child(2)')?.textContent?.trim() || 'image';
-//   let imageHtml = '';
-//   if (image) {
-//     const img = image.querySelector('img');
-//     img.removeAttribute('width');
-//     img.removeAttribute('height');
-//     img.setAttribute('alt', altText);
-//     imageHtml = `
-//             <div class="teaser-image">
-//                 ${image.outerHTML}
-//             </div>
-//         `;
-//   }
-
-//   const pretitle = getContent(block, 'div:nth-child(3) div');
-//   const title = getContent(block, 'div:nth-child(4) p');
-//   const description = block.querySelector('div:nth-child(5) div');
-//   const target = block.querySelector('div:nth-child(6)')?.textContent?.trim() || '_self';
-//   const ctaLink = description.querySelector('.button-container');
-
-//   let ctaHtml = '';
-//   if (ctaLink) {
-//     ctaLink.querySelector('a').setAttribute('target', target);
-//     ctaHtml = ctaLink.innerHTML;
-//     ctaLink.remove();
-//   }
-
-//   block.innerHTML = `
-//         <div class="teaser-card">
-//             ${imageHtml}
-//             <div class="teaser-content">
-//                 <div>
-//                     ${(pretitle) ? `<div class="teaser-pretitle">${pretitle}</div>` : ''}
-//                     ${(title) ? `<div class="teaser-title"><h3>${title}</h3></div>` : ''}
-//                     ${(description?.innerHTML) ? `<div class="teaser-description">${description.innerHTML}</div>` : ''}
-//                 </div>
-//                 ${(ctaHtml) ? `<div class="teaser-actions">${ctaHtml}</div>` : ''}
-//             </div>
-//         </div>
-//     `;
-}
-
-export function getTeaserHTML(block) {
   const image = block.querySelector('picture');
   const altText = block.querySelector('div:nth-child(2)')?.textContent?.trim() || 'image';
   let imageHtml = '';
@@ -61,9 +15,10 @@ export function getTeaserHTML(block) {
             </div>
         `;
   }
-  const pretitle = getContent(block, 'div:nth-child(3) p');
+
+  const pretitle = getContent(block, 'div:nth-child(3) div');
   const title = getContent(block, 'div:nth-child(4) p');
-  const description = block.querySelector('div:nth-child(5)');
+  const description = block.querySelector('div:nth-child(5) div');
   const target = block.querySelector('div:nth-child(6)')?.textContent?.trim() || '_self';
   const ctaLink = description.querySelector('.button-container');
 
@@ -74,12 +29,12 @@ export function getTeaserHTML(block) {
     ctaLink.remove();
   }
 
-  return `
+  block.innerHTML = `
         <div class="teaser-card">
             ${imageHtml}
             <div class="teaser-content">
                 <div>
-                    ${(pretitle) ? `<div class="teaser-pretitle"><p>${pretitle}</p></div>` : ''}
+                    ${(pretitle) ? `<div class="teaser-pretitle">${pretitle}</div>` : ''}
                     ${(title) ? `<div class="teaser-title"><h3>${title}</h3></div>` : ''}
                     ${(description?.innerHTML) ? `<div class="teaser-description">${description.innerHTML}</div>` : ''}
                 </div>
