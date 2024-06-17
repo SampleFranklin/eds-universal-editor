51c80cd2a2a9c4eb94c232cf339b1e26d2508a4c
Madhur <madhur.madan@nagarro.com>
2024-06-17 14:18:00 +0530

    Update Finance and Services

--- blocks/teaser-list/teaser-list.js
+++ blocks/teaser-list/teaser-list.js
@@ -1,4 +1,5 @@
 export default function decorate(block) {
+  block.classList.add('finance-service');
   const commonTitle = block.querySelector('.teaser-list > div:first-child > div > p').innerText;
   const teasers = Array.from(block.querySelectorAll('.teaser-list > div:not(:first-child)')).map((card) => {
     const imgSrc = card.querySelector('img')?.src;
@@ -7,31 +8,78 @@ export default function decorate(block) {
     const title = card.querySelector('div:nth-of-type(4) > p')?.innerText;
     const description = Array.from(card.querySelectorAll('div:nth-of-type(5) p:not(.button-container)')).map((p) => p.innerText).join(' ');
 
-    const ctaElements = card.querySelectorAll('div:nth-of-type(5) p.button-container a');
-    const ctas = Array.from(ctaElements).map((cta) => cta.outerHTML);
+   // const ctaElements = card.querySelectorAll('div:nth-of-type(5) p.button-container a');
+   // const ctas = Array.from(ctaElements).map((cta) => cta.outerHTML);
 
+    const ctaElements = card.querySelectorAll('div:nth-of-type(5) p.button-container a');
+    const ctas = Array.from(ctaElements).map((cta, index) => {
+        const buttonText = cta.textContent.trim();
+        if (index === 0) {
+            return `<button class="btn btn--primary-solid" href="#">${buttonText}</button>`;
+        } else if (index === 1) {
+            return `<a class="btn--link btn--link-primary" href="#">${buttonText} <span class="test-icon" ></span></a>`;
+        }
+    });
+ //  ${ctas.map((cta, index) => `<div class="teaser-action teaser-action-${index}">${cta}</div>`).join('')}
+    card.outerHTML = card.innerHTML;
     card.innerHTML = `
-            <div style="border: 1px solid #ccc; margin: 10px; padding: 10px;">
-                ${(imgSrc) ? `<div class="image-container"><img src="${imgSrc}" alt="${altText || title}" width="750" height="422" loading="lazy"></div>` : ''}
-                <div class="teaser-content">
-                  ${(preTitle) ? `<div class="teaser-pretitle"><p>${preTitle}</p></div>` : ''}
-                  ${(title) ? `<div class="teaser-title"><h3>${title}</h3></div>` : ''}
-                  ${(description) ? `<div class="teaser-description"><p>${description}</p></div>` : ''}
-                  ${ctas.map((cta, index) => `<div class="teaser-action teaser-action-${index}">${cta}</div>`).join('')}
+            <div class="finance-service__card">
+                <div class="finance-service__card__image">
+                    ${(imgSrc) ? `<div class="image-container"><img src="${imgSrc}" alt="${altText || title}" loading="lazy"></div>` : ''}
+                </div>
+                <div class="finance-service__card__content">
+                  <div class="finance-service__card__description">
+                      ${(preTitle) ? `<p class="finance-service__card__pretitle">${preTitle}</p>` : ''}
+                      ${(title) ? `<h3>${title}</h3>` : ''}
+                      ${(description) ? `<p class="finance-service__card__info">${description}</p>` : ''}
+                  </div>
+                  <div class="finance-service__card__actions">
+                    ${ctas.map((cta, index) => `${cta}`).join('')}
+                  </div>
                 </div>
             </div>
+
         `;
-    return card.outerHTML;
+    return card.innerHTML;
   });
 
   const newHtml = `
-        <div>
-            <h1>${commonTitle}</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-sm-8 col-sm-10">
+                <h2 class="text-color-tertiary">
+                    ${commonTitle}
+                </h2>
+            </div>
         </div>
-        <div style="display: flex;">
-            ${teasers.join('')}
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="finance-service__cards">
+                     ${teasers.join('')}
+                </div>
+            </div>
         </div>
+    </div>
     `;
 
   block.innerHTML = newHtml;
+  toggleFocusedClass();
 }
+
+
+function toggleFocusedClass() {
+
+    var cards = document.querySelectorAll('.finance-service__cards .finance-service__card');
+    cards[0].classList.add('finance-service__card--focused');
+
+    cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            cards.forEach(function(card) {
+                card.classList.remove('finance-service__card--focused');
+            });
+            this.classList.add('finance-service__card--focused');
+        });
+    });
+}
+
+
