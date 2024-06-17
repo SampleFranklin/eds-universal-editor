451b9a61183e89e12edc0d58fd96eb6f1fd63251
karrireddy <karri.reddy@nagarro.com>
2024-06-17 21:00:57 +0530

    update teaser

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,88 +1,102 @@
+import { sanitizeHtml } from "../../scripts/utils.js";
+
 export default function decorate(block) {
-//   const [imageEl, altTextEl, pretitleEl, titleEl, descriptionEl, targetEl, styleEl] = block.children;
-//   const image = imageEl?.querySelector('picture');
-//   if (image) {
-//     const img = image.querySelector('img');
-//     img.removeAttribute('width');
-//     img.removeAttribute('height');
-//     const alt = altTextEl?.textContent?.trim() || 'image';
-//     img.setAttribute('alt', alt);
-//   }
+  getTeaser(block);
+}
 
-//   const pretitle = pretitleEl?.textContent?.trim();
-//   const title = titleEl?.textContent?.trim();
-//   const description = Array.from(descriptionEl.querySelectorAll('p:not(.button-container)')).map((p) => p.outerHTML).join('');
-//   const target = targetEl?.textContent?.trim() || '_self';
-//   const cta = descriptionEl?.querySelector('.button-container a');
-//   cta?.setAttribute('target', target);
-//   cta?.classList.add('guide__cta');
-//   const style = styleEl?.textContent?.trim() || 'default-teaser';
+export function getTeaser(block) {
+  const [
+    imageEl,
+    altTextEl,
+    pretitleEl,
+    titleEl,
+    descriptionEl,
+    primaryCtaTextEl,
+    primaryCtaLinkEl,
+    primaryCtaTargetEl,
+    secondaryCtaTextEl,
+    secondaryCtaLinkEl,
+    secondaryCtaTargetEl,
+    styleEl
+  ] = block.children;
+  const image = imageEl?.querySelector('picture');
+  if (image) {
+    initImage(image, altTextEl)
+  }
 
-//   if(cta && 'black-teaser' === style) {
-//     const ctaText = cta.textContent?.trim();
-//     cta.innerHTML = `
-//         <p>${ctaText}</p>
-//         <div class='arrow_icon'></div>
-//     `;
-//   }
+  const pretitle = pretitleEl?.textContent?.trim();
+  const title = titleEl?.textContent?.trim();
+  const description = Array.from(descriptionEl.querySelectorAll('p')).map((p) => p.outerHTML).join('');
+  const styleText = styleEl?.textContent?.trim();
+  let style = [];
+  if(styleText) {
+    style = styleText.split(',');
+  } else {
+    style = ['light-teaser', 'buyers-guide-teaser'];
+  }
 
-//   block.classList.add(style);
-//   block.innerHTML = `
-//         <div class="teaser__card">
-//             ${(image) ? `<div class="teaser__image">${image.outerHTML}</div>` : ''}
-//             <div class="teaser__content">
-//                 <div>
-//                     ${(pretitle) ? `<div class="teaser__pretitle"><p>${pretitle}</p></div>` : ''}
-//                     ${(title) ? `<div class="teaser__title"><h3>${title}</h3></div>` : ''}
-//                     ${(description) ? `<div class="teaser__description">${description}</div>` : ''}
-//                 </div>
-//                 ${(cta) ? `<div class="teaser-actions">${cta.outerHTML}</div>` : ''}
-//             </div>
-//         </div>
-//     `;
-// }
+  const primaryCta = getCta(primaryCtaLinkEl, primaryCtaTargetEl);
+  const secondaryCta = getCta(secondaryCtaLinkEl, secondaryCtaTargetEl);
 
-// export function getTeaser(block) {
-//   const [imageEl, altTextEl, pretitleEl, titleEl, descriptionEl, targetEl, styleEl] = block.children;
-//   const image = imageEl?.querySelector('picture');
-//   if (image) {
-//     const img = image.querySelector('img');
-//     img.removeAttribute('width');
-//     img.removeAttribute('height');
-//     const alt = altTextEl?.textContent?.trim() || 'image';
-//     img.setAttribute('alt', alt);
-//   }
+  if (primaryCta) {
+    const primaryText = primaryCtaTextEl?.textContent?.trim() || '';
+    primaryCta?.classList.add('primary__btn');
+    primaryCta.innerHTML = '';
+    if (style) {
+      // primaryCta.insertAdjacentHTML('beforeend', `
+      //     <p>${primaryText}</p>
+      //     <div class='arrow_icon'></div>
+      // `);
+    } else {
+      primaryCta.insertAdjacentHTML('beforeend', primaryText);
+    }
+  }
 
-//   const pretitle = pretitleEl?.textContent?.trim();
-//   const title = titleEl?.textContent?.trim();
-//   const description = Array.from(descriptionEl.querySelectorAll('p:not(.button-container)')).map((p) => p.outerHTML).join('');
-//   const target = targetEl?.textContent?.trim() || '_self';
-//   const cta = descriptionEl?.querySelector('.button-container a');
-//   cta?.setAttribute('target', target);
-//   cta?.classList.add('guide__cta');
-//   const style = styleEl?.textContent?.trim() || 'default-teaser';
+  if (secondaryCta) {
+    secondaryCta.innerHTML = ''
+    secondaryCta.insertAdjacentHTML('beforeend', secondaryCtaTextEl?.textContent?.trim() || '');
+    secondaryCta.classList.add('secondary__btn');
+  }
 
-//   if(cta && 'black-teaser' === style) {
-//     const ctaText = cta.textContent?.trim();
-//     cta.innerHTML = `
-//         <p>${ctaText}</p>
-//         <div class='arrow_icon'></div>
-//     `;
-//   }
+  let ctaHtml = ``
+  if (primaryCta || secondaryCta) {
+    ctaHtml = `
+      <div class="teaser-actions">
+        ${(primaryCta) ? primaryCta.outerHTML : ''}
+        ${(secondaryCta) ? secondaryCta.outerHTML : ''}
+      </div>
+    `
+  }
+  block.classList.add(...style);
+  block.innerHTML = '';
+  block.insertAdjacentHTML('beforeend',
+    sanitizeHtml(`
+        <div class="teaser__card">
+            ${(image) ? `<div class="teaser__image">${image.outerHTML}</div>` : ''}
+            <div class="teaser__content">
+                <div class="teaser__info">
+                    ${(pretitle) ? `<div class="teaser__pretitle"><p>${pretitle}</p></div>` : ''}
+                    ${(title) ? `<div class="teaser__title"><h3>${title}</h3></div>` : ''}
+                    ${(description) ? `<div class="teaser__description">${description}</div>` : ''}
+                </div>
+                ${ctaHtml}
+            </div>
+        </div>
+  `));
+  return block;
+}
 
-//   block.classList.add(style);
-//   block.innerHTML = `
-//         <div class="teaser__card">
-//             ${(image) ? `<div class="teaser__image">${image.outerHTML}</div>` : ''}
-//             <div class="teaser__content">
-//                 <div>
-//                     ${(pretitle) ? `<div class="teaser__pretitle"><p>${pretitle}</p></div>` : ''}
-//                     ${(title) ? `<div class="teaser__title"><h3>${title}</h3></div>` : ''}
-//                     ${(description) ? `<div class="teaser__description">${description}</div>` : ''}
-//                 </div>
-//                 ${(cta) ? `<div class="teaser-actions">${cta.outerHTML}</div>` : ''}
-//             </div>
-//         </div>
-//     `;
-//   return block;
+function getCta(linkEl, targetEl) {
+  const link = linkEl?.querySelector('.button-container a');
+  const target = targetEl?.textContent?.trim() || '_self';
+  link?.setAttribute('target', target);
+  return link;
+}
+
+function initImage(image, altTextEl) {
+  const img = image.querySelector('img');
+  img.removeAttribute('width');
+  img.removeAttribute('height');
+  const alt = altTextEl?.textContent?.trim() || 'image';
+  img.setAttribute('alt', alt);
 }
