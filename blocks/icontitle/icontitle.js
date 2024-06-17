6f06576b39ed7a8b8fea595b0a8d4715ba876c65
varunmaisuria <varun.maisuria@nagarro.com>
2024-06-17 11:50:36 +0530

    icontitlejs update

--- blocks/icontitle/icontitle.js
+++ blocks/icontitle/icontitle.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
     console.log("icontitle: "+ block.children)
     const icons = block.querySelectorAll('.icontitle picture');
-    const icon = icons[0]
+    const icon = icons[0];
     const iconClicked = icons[1];
     const title = block.querySelector('div:nth-child(1) > p').textContent.trim();
     const iconImages = block.querySelectorAll('.icontitle img');
