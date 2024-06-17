07bc54156e1f385140b7580b902415f59af67f73
karrireddy <karri.reddy@nagarro.com>
2024-06-17 14:11:23 +0530

    update

--- blocks/contact/contact.js
+++ blocks/contact/contact.js
@@ -1,28 +1,30 @@
-export default function decorate(block) {
-    const commonTitle = block.querySelector('.contact > div:first-child > div > p').innerText;
-    const ctaElements = Array.from(block.querySelectorAll('.contact > div:not(:first-child)')).map(element => {
-        const imgSrc = element.querySelector('img')?.src;
-        const altText = element.querySelector('div:nth-of-type(2) > p')?.innerText;
-        const link = element.querySelector('div:nth-of-type(4) p.button-container a');
-        console.log("link",link)
-        const target = element.querySelector('div:nth-of-type(5) > p')?.innerText;
-        // console.log("target",target);
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
-        element.innerHTML = `
-                <a href="${link}" target="${target}" class="user__contact--icon">
-                    <img src="${imgSrc}" alt="${altText || ""}" loading="lazy">
-                </a>
-        `;
-        return element.innerHTML;
-    });
-    const newHtml = `
+export default function decorate(block) {
+  const [titleEl, ...ctasEl] = block.children;
+  const title = titleEl?.textContent?.trim();
+  const ctaElements = ctasEl.map((element) => {
+    const [imageEl, altTextEl, ctaTextEl, linkEl, targetEl] = element.children;
+    const imgSrc = imageEl?.querySelector('img')?.src;
+    const altText = altTextEl?.textContent?.trim() || 'icon';
+    const ctaText = ctaTextEl?.textContent?.trim() || '';
+    const link = linkEl?.querySelector('.button-container a')?.href;
+    const target = targetEl?.textContent?.trim() || '_self';
+
+    element.innerHTML = `
+      <a href="${link}" target="${target}" class="user__contact--icon" title=${ctaText}>
+          <img src="${imgSrc}" alt="${altText}" loading="lazy">
+      </a>
+    `;
+    moveInstrumentation(element, element.firstElementChild);
+    return element.innerHTML;
+  }).join('');
+  block.innerHTML = `
         <div class="user__contact">
-            <h4>${commonTitle}</h4>
+            ${(title) ? `<h4>${title}</h4>` : ''}
             <div class="user__contact__icons">
-                ${ctaElements.join('')}
+                ${ctaElements}
             </div>
         </div>
     `;
-    block.innerHTML = newHtml;
-    console.log(block);
 }
