46ad47a2f5062698fc86a9ea7d615d8fc0626cae
karrireddy <karri.reddy@nagarro.com>
2024-06-17 21:03:40 +0530

    update teaser list

--- blocks/teaser-list/teaser-list.js
+++ blocks/teaser-list/teaser-list.js
@@ -1,46 +1,49 @@
+import { getTeaser } from "../teaser/teaser.js";
+
 export default function decorate(block) {
   block.classList.add('finance-service');
   const commonTitle = block.querySelector('.teaser-list > div:first-child > div > p').innerText;
   const teasers = Array.from(block.querySelectorAll('.teaser-list > div:not(:first-child)')).map((card) => {
-    const imgSrc = card.querySelector('img')?.src;
-    const altText = card.querySelector('div:nth-of-type(2) > p')?.innerText;
-    const preTitle = card.querySelector('div:nth-of-type(3) > p')?.innerText;
-    const title = card.querySelector('div:nth-of-type(4) > p')?.innerText;
-    const description = Array.from(card.querySelectorAll('div:nth-of-type(5) p:not(.button-container)')).map((p) => p.innerText).join(' ');
+    return getTeaser(card).outerHTML;
+//     const imgSrc = card.querySelector('img')?.src;
+//     const altText = card.querySelector('div:nth-of-type(2) > p')?.innerText;
+//     const preTitle = card.querySelector('div:nth-of-type(3) > p')?.innerText;
+//     const title = card.querySelector('div:nth-of-type(4) > p')?.innerText;
+//     const description = Array.from(card.querySelectorAll('div:nth-of-type(5) p:not(.button-container)')).map((p) => p.innerText).join(' ');
 
-   // const ctaElements = card.querySelectorAll('div:nth-of-type(5) p.button-container a');
-   // const ctas = Array.from(ctaElements).map((cta) => cta.outerHTML);
+//    // const ctaElements = card.querySelectorAll('div:nth-of-type(5) p.button-container a');
+//    // const ctas = Array.from(ctaElements).map((cta) => cta.outerHTML);
 
-    const ctaElements = card.querySelectorAll('div:nth-of-type(5) p.button-container a');
-    const ctas = Array.from(ctaElements).map((cta, index) => {
-        const buttonText = cta.textContent.trim();
-        if (index === 0) {
-            return `<button class="btn btn--primary-solid" href="#">${buttonText}</button>`;
-        } else if (index === 1) {
-            return `<a class="btn--link btn--link-primary" href="#">${buttonText} <span class="test-icon" ></span></a>`;
-        }
-    });
- //  ${ctas.map((cta, index) => `<div class="teaser-action teaser-action-${index}">${cta}</div>`).join('')}
-    card.outerHTML = card.innerHTML;
-    card.innerHTML = `
-            <div class="finance-service__card">
-                <div class="finance-service__card__image">
-                    ${(imgSrc) ? `<div class="image-container"><img src="${imgSrc}" alt="${altText || title}"></div>` : ''}
-                </div>
-                <div class="finance-service__card__content">
-                  <div class="finance-service__card__description">
-                      ${(preTitle) ? `<p class="finance-service__card__pretitle">${preTitle}</p>` : ''}
-                      ${(title) ? `<h3>${title}</h3>` : ''}
-                      ${(description) ? `<p class="finance-service__card__info">${description}</p>` : ''}
-                  </div>
-                  <div class="finance-service__card__actions">
-                    ${ctas.map((cta, index) => `${cta}`).join('')}
-                  </div>
-                </div>
-            </div>
+//     const ctaElements = card.querySelectorAll('div:nth-of-type(5) p.button-container a');
+//     const ctas = Array.from(ctaElements).map((cta, index) => {
+//         const buttonText = cta.textContent.trim();
+//         if (index === 0) {
+//             return `<button class="btn btn--primary-solid" href="#">${buttonText}</button>`;
+//         } else if (index === 1) {
+//             return `<a class="btn--link btn--link-primary" href="#">${buttonText} <span class="test-icon" ></span></a>`;
+//         }
+//     });
+//  //  ${ctas.map((cta, index) => `<div class="teaser-action teaser-action-${index}">${cta}</div>`).join('')}
+//     card.outerHTML = card.innerHTML;
+//     card.innerHTML = `
+//             <div class="finance-service__card">
+//                 <div class="finance-service__card__image">
+//                     ${(imgSrc) ? `<div class="image-container"><img src="${imgSrc}" alt="${altText || title}"></div>` : ''}
+//                 </div>
+//                 <div class="finance-service__card__content">
+//                   <div class="finance-service__card__description">
+//                       ${(preTitle) ? `<p class="finance-service__card__pretitle">${preTitle}</p>` : ''}
+//                       ${(title) ? `<h3>${title}</h3>` : ''}
+//                       ${(description) ? `<p class="finance-service__card__info">${description}</p>` : ''}
+//                   </div>
+//                   <div class="finance-service__card__actions">
+//                     ${ctas.map((cta, index) => `${cta}`).join('')}
+//                   </div>
+//                 </div>
+//             </div>
 
-        `;
-    return card.innerHTML;
+//         `;
+//     return card.innerHTML;
   });
 
   const newHtml = `
@@ -63,7 +66,7 @@ export default function decorate(block) {
     `;
 
   block.innerHTML = newHtml;
-  toggleFocusedClass();
+//   toggleFocusedClass();
   mobileLazyLoading();
 }
 
@@ -76,47 +79,47 @@ function mobileLazyLoading(){
 }
 
 
-function toggleFocusedClass() {
+// function toggleFocusedClass() {
 
-    var cards = document.querySelectorAll('.finance-service__cards .finance-service__card');
-    cards[0].classList.add('finance-service__card--focused');
+//     var cards = document.querySelectorAll('.finance-service__cards .finance-service__card');
+//     cards[0].classList.add('finance-service__card--focused');
 
-    cards.forEach(function(card) {
-        card.addEventListener('click', function() {
-            cards.forEach(function(card) {
-                card.classList.remove('finance-service__card--focused');
-            });
-            this.classList.add('finance-service__card--focused');
-        });
-    });
+//     cards.forEach(function(card) {
+//         card.addEventListener('click', function() {
+//             cards.forEach(function(card) {
+//                 card.classList.remove('finance-service__card--focused');
+//             });
+//             this.classList.add('finance-service__card--focused');
+//         });
+//     });
 
-// Sroll the cards on click
-    const teaserCards = document.querySelectorAll('.finance-service__card');
+// // Sroll the cards on click
+//     const teaserCards = document.querySelectorAll('.finance-service__card');
 
-    teaserCards.forEach(teaserCard => {
-        teaserCard.addEventListener('click', () => {
-            teaserCards.forEach(c => c.classList.remove('finance-service__card--focused'));
-            teaserCard.classList.add('finance-service__card--focused');
+//     teaserCards.forEach(teaserCard => {
+//         teaserCard.addEventListener('click', () => {
+//             teaserCards.forEach(c => c.classList.remove('finance-service__card--focused'));
+//             teaserCard.classList.add('finance-service__card--focused');
 
-            const focusedTeaserCard = document.querySelector('.finance-service__card--focused');
+//             const focusedTeaserCard = document.querySelector('.finance-service__card--focused');
 
-            if (focusedTeaserCard) {
-                const container = focusedTeaserCard.closest('.finance-service__cards');
-                const cardOffsetLeft = focusedTeaserCard.offsetLeft;
-                const containerOffsetLeft = container.offsetLeft;
-                const scrollLeft = cardOffsetLeft - containerOffsetLeft;
-                const cardWidth = focusedTeaserCard.offsetWidth;
-                const containerWidth = container.clientWidth;
-                const maxScrollLeft = Math.min(scrollLeft, container.scrollWidth - containerWidth);
+//             if (focusedTeaserCard) {
+//                 const container = focusedTeaserCard.closest('.finance-service__cards');
+//                 const cardOffsetLeft = focusedTeaserCard.offsetLeft;
+//                 const containerOffsetLeft = container.offsetLeft;
+//                 const scrollLeft = cardOffsetLeft - containerOffsetLeft;
+//                 const cardWidth = focusedTeaserCard.offsetWidth;
+//                 const containerWidth = container.clientWidth;
+//                 const maxScrollLeft = Math.min(scrollLeft, container.scrollWidth - containerWidth);
 
-                container.scrollTo({
-                    left: maxScrollLeft,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
+//                 container.scrollTo({
+//                     left: maxScrollLeft,
+//                     behavior: 'smooth'
+//                 });
+//             }
+//         });
+//     });
 
-}
+// }
 
 
