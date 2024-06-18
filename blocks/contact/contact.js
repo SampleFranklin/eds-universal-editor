b233ad6aaabfe912497c774398798c466beefa4a
karrireddy <karri.reddy@nagarro.com>
2024-06-18 17:07:07 +0530

    eslint fix

--- blocks/contact/contact.js
+++ blocks/contact/contact.js
@@ -1,24 +1,21 @@
 export default function decorate(block) {
   const commonTitle = block.querySelector(
-    ".contact > div:first-child > div > p"
+    '.contact > div:first-child > div > p',
   ).innerText;
   const ctaElements = Array.from(
-    block.querySelectorAll(".contact > div:not(:first-child)")
+    block.querySelectorAll('.contact > div:not(:first-child)'),
   ).map((element) => {
-    const imgSrc = element.querySelector("img")?.src;
-    const altText = element.querySelector("div:nth-of-type(2) > p")?.innerText;
+    const imgSrc = element.querySelector('img')?.src;
+    const altText = element.querySelector('div:nth-of-type(2) > p')?.innerText;
     const link = element.querySelector(
-      "div:nth-of-type(4) p.button-container a"
+      'div:nth-of-type(4) p.button-container a',
     );
-    console.log("link", link);
-    const target = element.querySelector("div:nth-of-type(5) > p")?.innerText;
-    // console.log("target",target);
-
+    const target = element.querySelector('div:nth-of-type(5) > p')?.innerText;
     element.innerHTML = `
                   <a href="${link}" target="${target}" class="user__contact--icon">
                       <img src="${imgSrc}" alt="${
-      altText || ""
-    }" loading="lazy">
+  altText || ''
+}" loading="lazy">
                   </a>
           `;
     return element.innerHTML;
@@ -27,7 +24,7 @@ export default function decorate(block) {
           <div class="user__contact">
               <h4>${commonTitle}</h4>
               <div class="user__contact__icons">
-                  ${ctaElements.join("")}
+                  ${ctaElements.join('')}
               </div>
           </div>
       `;
