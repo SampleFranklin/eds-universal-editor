450c7982bed97337e52db6e4c926c9d589688608
Madhur <madhur.madan@nagarro.com>
2024-06-19 17:59:18 +0530

    Refactoring of code

--- utility/ctaUtils.js
+++ utility/ctaUtils.js
@@ -1,23 +1,20 @@
-import { sanitizeHtml } from '../../scripts/utils.js';
+import { utility } from './utility.js';
 
-class CTA {
-  static getLink(linkEl, textEl, targetEl, className) {
+export const ctaUtils = {
+
+  getLink(linkEl, textEl, targetEl, className) {
     const link = linkEl?.querySelector('.button-container a');
     const target = targetEl?.textContent?.trim() || '_self';
     link?.setAttribute('target', target);
-    return this.getLinkText(link, textEl, className);
-  }
-
-  static getLinkText(link, textEl, className) {
     const text = textEl?.textContent?.trim() || '';
     if (link) {
       link.innerHTML = '';
-      link.insertAdjacentHTML('beforeend', sanitizeHtml(text));
+      link.insertAdjacentHTML('beforeend', utility.sanitizeHtml(text));
       if (className && link) {
         link.classList.add(className);
       }
     }
     return link;
-  }
-}
-export default CTA;
+  },
+
+};
--- utility/utility.js
+++ utility/utility.js
@@ -1,5 +1,5 @@
-class Utility {
-  static mobileLazyLoading(element, imgSelector) {
+export const utility = {
+  mobileLazyLoading(element, imgSelector) {
     const isMobile = window.matchMedia('(max-width: 768px)').matches;
     const imgElement = element.querySelector(imgSelector);
     if (isMobile && imgElement) {
@@ -7,7 +7,14 @@ class Utility {
     } else if (!isMobile && imgElement) {
       imgElement.setAttribute('loading', 'eager');
     }
-  }
-}
+  },
+  isInternalLink(href) {
+    return !/^https?:\/\//i.test(href);
+  },
+  sanitizeHtml(html) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    return doc.body.innerHTML;
+  },
 
-export default Utility;
+};
