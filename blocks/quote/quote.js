6863b97f5b824b9d6e42ad2989e4ca1c8740a331
karrireddy <karri.reddy@nagarro.com>
2024-06-25 17:07:30 +0530

    quote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -4,4 +4,12 @@ export default function decorate(block) {
   // const blockquote = document.createElement('blockquote');
   // blockquote.textContent = quoteWrapper.textContent.trim();
   // quoteWrapper.replaceChildren(blockquote);
+    block.insertAdjacentHTML(
+      'beforeend',
+      `
+        <div>
+          <img src={{publish_domain}} alt="image">
+        </div>
+      `
+    )
 }
