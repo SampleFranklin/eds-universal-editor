248af904b30cb90f9c94844863854d6a51050e14
yadalavineetha <yadala.vineetha@nagarro.com>
2024-06-27 19:11:48 +0530

    Nexa-world component

--- blocks/nexa-world-component/nexa-world-component.js
+++ blocks/nexa-world-component/nexa-world-component.js
@@ -1,3 +1,7 @@
 export default function decorate(block){
   console.log("vineetha")
+  
+  const imgElement =block.querySelector('data-aue-prop="icon"');
+  console.log("imgElement",imgElement)
+  
 }
