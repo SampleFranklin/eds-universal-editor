d6d577afd9bcc3c091773f7500064459ec44c5cd
Priyansh <priyansh.ankre@nagarro.com>
2024-07-02 13:51:09 +0530

    eslint fix for arena-hero-banner

--- blocks/banner-carousel/banner-carousel.js
+++ blocks/banner-carousel/banner-carousel.js
@@ -1,9 +1,10 @@
-import ctaUtils from "../../utility/ctaUtils.js";
+import ctaUtils from '../../utility/ctaUtils.js';
+import { fetchPlaceholders } from '../../scripts/aem.js';
 // eslint no-underscore-dangle: 0
 
-async function fetchCar() {
+async function fetchCar(domain) {
   const car = await fetch(
-    "https://publish-p71852-e1137339.adobeaemcloud.com/graphql/execute.json/msil-platform/arenaBannerList?q=123"
+    `${domain}/graphql/execute.json/msil-platform/arenaBannerList?q=123`,
   );
   return car.json();
 }
@@ -14,20 +15,21 @@ function convertToLakh(number) {
 }
 
 export default async function decorate(block) {
-  const carResponse = await fetchCar();
+  const { publishDomain } = await fetchPlaceholders();
+  const carResponse = await fetchCar(publishDomain);
   const carsObject = carResponse?.data?.carModelList?.items?.reduce(
     (acc, car) => {
       acc[car.modelId] = car;
       return acc;
     },
-    {}
+    {},
   );
 
-  const carContainersWrapper = document.createElement("div");
+  const carContainersWrapper = document.createElement('div');
 
   const [bGImgContainer, ...carList] = block.children;
 
-  const bgImg = bGImgContainer.querySelector("picture");
+  const bgImg = bGImgContainer.querySelector('picture');
 
   carList.forEach((element) => {
     const [
@@ -45,59 +47,59 @@ export default async function decorate(block) {
 
     const carObjectItem = carsObject[modelId.textContent];
 
-    const [firstLetterTitle, ...rest] = title.textContent.split(" ");
-    const restTitle = rest.join(" ");
+    const [firstLetterTitle, ...rest] = title.textContent.split(' ');
+    const restTitle = rest.join(' ');
 
     const primaryCta = ctaUtils.getLink(
       primaryCtaLinkEl,
       primaryCtaTextEl,
       primaryCtaTargetEl,
-      "primary__btn"
+      'primary__btn',
     );
     const secondaryCta = ctaUtils.getLink(
       secondaryCtaLinkEl,
       secondaryCtaTextEl,
       secondaryCtaTargetEl,
-      "secondary__btn"
+      'secondary__btn',
     );
     carContainersWrapper.innerHTML += `
       <div class="car-container">
           <img
-            src=${carObjectItem?.carImage._publishUrl || ""}
-            alt=${carObjectItem?.carName || ""}
+            src=${carObjectItem?.carImage._publishUrl || ''}
+            alt=${carObjectItem?.carName || ''}
             class="sidebar-car--image"
           />
           <div class="sidebar">
             <div class="sidebar_text_container">
               <div class="text-container">
-                <span>${firstLetterTitle || ""} ${restTitle || ""}</span>
+                <span>${firstLetterTitle || ''} ${restTitle || ''}</span>
               </div>
               <img
-                src=${carObjectItem?.carLogoImage._publishUrl || ""}
-                alt=${carObjectItem?.carName || ""}
+                src=${carObjectItem?.carLogoImage._publishUrl || ''}
+                alt=${carObjectItem?.carName || ''}
                 class="sidebar-car--logo"
               />
               <span><strong>${carObjectItem?.bodyType}</strong> | ${
-      type?.textContent || ""
-    }</span>
+  type?.textContent || ''
+}</span>
               <div class="sidebar--hr"></div>
               <div class="sidebar--details">
                 <div class="sidebar--details--exshowroom">
                   <span>Ex. showroom:</span>
                   <span><strong>${
-                    convertToLakh(carObjectItem?.exShowroomPrice) || ""
-                  } Lakhs</strong></span>
+  convertToLakh(carObjectItem?.exShowroomPrice) || ''
+} Lakhs</strong></span>
                 </div>
                 <div class="sidebar--details--onroad">
                   <span>Estd. On-road in Gurgaon:</span>
                   <span><strong>${
-                    onRoadPrice?.textContent || ""
-                  }</strong></span>
+  onRoadPrice?.textContent || ''
+}</strong></span>
                 </div>
               </div>
               <div class="buttons">
-                ${primaryCta ? primaryCta.outerHTML : ""}
-                ${secondaryCta ? secondaryCta.outerHTML : ""}
+                ${primaryCta ? primaryCta.outerHTML : ''}
+                ${secondaryCta ? secondaryCta.outerHTML : ''}
               </div>
             </div>
           </div>
@@ -118,12 +120,12 @@ export default async function decorate(block) {
 
   block.innerHTML = heroBannerWrapper;
 
-  const nxtBtn = document.querySelector(".nxt-btn");
-  const preBtn = document.querySelector(".pre-btn");
+  const nxtBtn = document.querySelector('.nxt-btn');
+  const preBtn = document.querySelector('.pre-btn');
   preBtn.disabled = true;
 
   const isDesktop = window.innerWidth > 998;
-  const cards = document.querySelectorAll(".car-container");
+  const cards = document.querySelectorAll('.car-container');
   const cardCount = cards.length;
   let currentIndex = 0;
   const cardsPerPage = isDesktop ? 3 : 1;
@@ -135,72 +137,72 @@ export default async function decorate(block) {
 
   function addBGToSlider(indexSidebar, sideBarItem) {
     if (indexSidebar === 0) {
-      sideBarItem.classList.add("left_sidebar");
-      const img = document.createElement("img");
-      img.src = "../../icons/title_cover_blue.svg";
-      sideBarItem.querySelector(".text-container").prepend(img);
+      sideBarItem.classList.add('left_sidebar');
+      const img = document.createElement('img');
+      img.src = '../../icons/title_cover_blue.svg';
+      sideBarItem.querySelector('.text-container').prepend(img);
     } else if (indexSidebar === 1) {
-      sideBarItem.classList.add("mid_sidebar");
-      const img = document.createElement("img");
-      img.src = "../../icons/title_cover_yellow.svg";
-      sideBarItem.querySelector(".text-container").prepend(img);
+      sideBarItem.classList.add('mid_sidebar');
+      const img = document.createElement('img');
+      img.src = '../../icons/title_cover_yellow.svg';
+      sideBarItem.querySelector('.text-container').prepend(img);
     } else if (indexSidebar === 2) {
-      sideBarItem.classList.add("right_sidebar");
-      const img = document.createElement("img");
-      img.src = "../../icons/title_cover_yellow.svg";
-      sideBarItem.querySelector(".text-container").prepend(img);
+      sideBarItem.classList.add('right_sidebar');
+      const img = document.createElement('img');
+      img.src = '../../icons/title_cover_yellow.svg';
+      sideBarItem.querySelector('.text-container').prepend(img);
     }
   }
 
   function addImgToTitleDesktop(indexSidebar, sideBarItem) {
     if (indexSidebar === 0) {
-      const img = document.createElement("img");
-      img.src = "../../icons/title_cover_blue.svg";
-      sideBarItem.querySelector(".text-container").prepend(img);
+      const img = document.createElement('img');
+      img.src = '../../icons/title_cover_blue.svg';
+      sideBarItem.querySelector('.text-container').prepend(img);
     } else if (indexSidebar === 1) {
-      const img = document.createElement("img");
-      img.src = "../../icons/title_cover_yellow.svg";
-      sideBarItem.querySelector(".text-container").prepend(img);
+      const img = document.createElement('img');
+      img.src = '../../icons/title_cover_yellow.svg';
+      sideBarItem.querySelector('.text-container').prepend(img);
     } else if (indexSidebar === 2) {
-      const img = document.createElement("img");
-      img.src = "../../icons/title_cover_yellow.svg";
-      sideBarItem.querySelector(".text-container").prepend(img);
+      const img = document.createElement('img');
+      img.src = '../../icons/title_cover_yellow.svg';
+      sideBarItem.querySelector('.text-container').prepend(img);
     }
   }
 
   function addImgToTitleMobile(sideBarItem) {
-    const img = document.createElement("img");
-    img.src = "../../icons/title_cover_yellow.svg";
-    sideBarItem.querySelector(".text-container").prepend(img);
+    const img = document.createElement('img');
+    img.src = '../../icons/title_cover_yellow.svg';
+    sideBarItem.querySelector('.text-container').prepend(img);
   }
 
   function handleMouseEnter(event) {
     if (highlightedSidebar) {
-      highlightedSidebar.classList.remove("highlight");
+      highlightedSidebar.classList.remove('highlight');
     }
 
     highlightedSidebar = event;
-    highlightedSidebar.classList.add("highlight");
+    highlightedSidebar.classList.add('highlight');
   }
 
   function handleMouseLeave() {
     if (highlightedSidebar) {
-      highlightedSidebar.classList.remove("highlight");
+      highlightedSidebar.classList.remove('highlight');
     }
     const middleIndex = currentIndex + Math.floor(cardsPerPage / 2);
     if (middleIndex < cards.length) {
       highlightedSidebar = cards[middleIndex];
-      highlightedSidebar.classList.add("highlight");
+      highlightedSidebar.classList.add('highlight');
     }
   }
 
-  function toggleDisableNxtPrvBtn(currentIndex, totalCount) {
-    if (currentIndex === 0) {
+  function toggleDisableNxtPrvBtn(isCurrentIndex, totalCount) {
+    if (isCurrentIndex === 0) {
       preBtn.disabled = true;
     } else {
       preBtn.disabled = false;
     }
-    if (currentIndex === totalCount - cardsPerPage) {
+    if (isCurrentIndex === totalCount - cardsPerPage) {
       nxtBtn.disabled = true;
     } else {
       nxtBtn.disabled = false;
@@ -210,24 +212,24 @@ export default async function decorate(block) {
   function showCards(index) {
     cards.forEach((card, i) => {
       const indexSidebar = i - index;
-      const sideBarItem = card.querySelector(".sidebar");
+      const sideBarItem = card.querySelector('.sidebar');
       if (isDesktop) {
         addBGToSlider(indexSidebar, sideBarItem);
         addImgToTitleDesktop(indexSidebar, sideBarItem);
       } else {
         addImgToTitleMobile(sideBarItem);
       }
-      card.classList.remove("show");
+      card.classList.remove('show');
       if (i >= index && i < index + cardsPerPage) {
-        card.classList.add("show");
+        card.classList.add('show');
         card
-          .querySelector(".sidebar-car--image")
-          .addEventListener("mouseenter", () => {
+          .querySelector('.sidebar-car--image')
+          .addEventListener('mouseenter', () => {
             handleMouseEnter(card);
           });
         card
-          .querySelector(".sidebar-car--image")
-          .addEventListener("mouseleave", () => {
+          .querySelector('.sidebar-car--image')
+          .addEventListener('mouseleave', () => {
             handleMouseLeave(card);
           });
       }
@@ -235,28 +237,28 @@ export default async function decorate(block) {
 
     const middleIndex = Math.floor(cardsPerPage / 2);
     if (index + middleIndex < cards.length) {
-      cards[index + middleIndex].classList.add("highlight");
+      cards[index + middleIndex].classList.add('highlight');
       highlightedSidebar = cards[index + middleIndex];
     }
   }
 
-  const bullets = document.createElement("div");
-  bullets.classList.add("bullets");
-  bullets.id = "bullets";
+  const bullets = document.createElement('div');
+  bullets.classList.add('bullets');
+  bullets.id = 'bullets';
 
   for (let i = 0; i < cardCount; i += cardsPerPage) {
     bullets.innerHTML += `<input id="bullet" type="radio" ${
-      i === 0 ? "checked" : ""
+      i === 0 ? 'checked' : ''
     } />`;
   }
 
-  document.querySelector(".hero_banner_container_wrapper").appendChild(bullets);
+  document.querySelector('.hero_banner_container_wrapper').appendChild(bullets);
 
-  nxtBtn.addEventListener("click", () => {
+  nxtBtn.addEventListener('click', () => {
     if (currentIndex + cardsPerPage < cards.length) {
       currentIndex += cardsPerPage;
       const pageCount = Math.floor(currentIndex / cardsPerPage);
-      const isBullets = document.querySelector("#bullets").children;
+      const isBullets = document.querySelector('#bullets').children;
       isBullets[pageCount - 1].checked = false;
       isBullets[pageCount].checked = true;
       toggleDisableNxtPrvBtn(currentIndex, cardCount);
@@ -264,11 +266,11 @@ export default async function decorate(block) {
     }
   });
 
-  preBtn.addEventListener("click", () => {
+  preBtn.addEventListener('click', () => {
     if (currentIndex - cardsPerPage >= 0) {
       currentIndex -= cardsPerPage;
       const pageCount = Math.floor(currentIndex / cardsPerPage);
-      const isBullets = document.querySelector("#bullets").children;
+      const isBullets = document.querySelector('#bullets').children;
       isBullets[pageCount + 1].checked = false;
       isBullets[pageCount].checked = true;
       toggleDisableNxtPrvBtn(currentIndex, cardCount);
