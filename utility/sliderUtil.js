91b98a4aadf2774e81b94e574354991ce685e401
Adarsh Chauhan <adarsh.chauhan@nagarro.com>
2024-07-02 17:38:14 +0530

    lint fix"

--- utility/sliderUtil.js
+++ utility/sliderUtil.js
@@ -1,8 +1,19 @@
 const slider = {
-  initSlider: function (sliderContainer, prevButton, nextButton, boxes) {
+  initSlider(sliderContainer, prevButton, nextButton, boxes) {
+    function calculateVisibleBoxes() {
+      const width = window.innerWidth;
+      if (width >= 900) {
+        return 3;
+      }
+      if (width >= 600) {
+        return 2;
+      }
+      return 1.5; // 1.5 slides on small screens
+    }
     let visibleBoxes = calculateVisibleBoxes();
     const totalBoxes = boxes.length;
     let currentIndex = 0;
+    // Calculate the number of visible boxes based on the window width
 
     const updateSlider = () => {
       const boxWidth = boxes[0].offsetWidth;
@@ -10,48 +21,36 @@ const slider = {
       sliderContainer.style.transform = `translateX(${offset}px)`;
     };
 
-    prevButton.addEventListener("click", () => {
-      nextButton.classList.remove("hide");
-      currentIndex =
-        currentIndex > 0 ? currentIndex - 3 : totalBoxes - visibleBoxes;
-      if (currentIndex >= 0) prevButton.classList.add("hide");
+    prevButton.addEventListener('click', () => {
+      nextButton.classList.remove('hide');
+      currentIndex = currentIndex > 0 ? currentIndex - 3 : totalBoxes - visibleBoxes;
+      if (currentIndex >= 0) prevButton.classList.add('hide');
 
       updateSlider();
     });
 
-    nextButton.addEventListener("click", () => {
-      prevButton.classList.remove("hide");
-      currentIndex =
-        currentIndex < totalBoxes - visibleBoxes ? currentIndex + 3 : 0;
-      if (currentIndex >= totalBoxes - 3) nextButton.classList.add("hide");
+    nextButton.addEventListener('click', () => {
+      prevButton.classList.remove('hide');
+      currentIndex = currentIndex < totalBoxes - visibleBoxes ? currentIndex + 3 : 0;
+      if (currentIndex >= totalBoxes - 3) nextButton.classList.add('hide');
 
       updateSlider();
     });
-
-    // Calculate the number of visible boxes based on the window width
-    function calculateVisibleBoxes() {
-      const width = window.innerWidth;
-      if (width >= 900) {
-        return 3;
-      } else if (width >= 600) {
-        return 2;
-      } else {
-        return 1.5; // 1.5 slides on small screens
-      }
-    }
     // Ensure slider adjusts on window resize
-    window.addEventListener("resize", () => {
-      boxWidth = boxes[0].offsetWidth;
+    window.addEventListener('resize', () => {
       visibleBoxes = calculateVisibleBoxes();
-      sliderContainer.style.transition = "none";
+      sliderContainer.style.transition = 'none';
       updateSlider();
       setTimeout(() => {
-        sliderContainer.style.transition = "transform 0.5s ease-in-out";
+        sliderContainer.style.transition = 'transform 0.5s ease-in-out';
       }, 0);
     });
 
     // Swipe functionality
-    let startX, startY, endX, endY;
+    let startX;
+    let startY;
+    let endX;
+    let endY;
 
     const handleTouchStart = (e) => {
       startX = e.touches[0].clientX;
@@ -71,20 +70,18 @@ const slider = {
         // Horizontal swipe
         if (diffX > 50) {
           // Swiped left
-          currentIndex =
-            currentIndex < totalBoxes - visibleBoxes ? currentIndex + 3 : 0;
+          currentIndex = currentIndex < totalBoxes - visibleBoxes ? currentIndex + 3 : 0;
         } else if (diffX < -50) {
           // Swiped right
-          currentIndex =
-            currentIndex > 0 ? currentIndex - 3 : totalBoxes - visibleBoxes;
+          currentIndex = currentIndex > 0 ? currentIndex - 3 : totalBoxes - visibleBoxes;
         }
         updateSlider();
       }
     };
 
-    sliderContainer.addEventListener("touchstart", handleTouchStart);
-    sliderContainer.addEventListener("touchmove", handleTouchMove);
-    sliderContainer.addEventListener("touchend", handleTouchEnd);
+    sliderContainer.addEventListener('touchstart', handleTouchStart);
+    sliderContainer.addEventListener('touchmove', handleTouchMove);
+    sliderContainer.addEventListener('touchend', handleTouchEnd);
 
     // Initialize slider
     updateSlider();
