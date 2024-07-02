a8676faf2d6a16e24560ed2339cc403b9bdfcd43
yadalavineetha <yadala.vineetha@nagarro.com>
2024-07-02 08:28:13 +0530

    Nexa-world component

--- blocks/nexa-world-component/nexa-world-component.js
+++ blocks/nexa-world-component/nexa-world-component.js
@@ -27,7 +27,7 @@ export default function decorate(block) {
     const links = Array.from(linkEls).map(linkEl => ({
       text: linkEl.textContent.trim(),
       href: linkEl.querySelector('a')?.href || '#',
-      target:linkEl.querySelector('a')?.target || '_self',
+      target: linkEl.textContent?.trim() || '_self',
       imgSrc: linkEl.getAttribute('data-img-src') || '', 
       imgAlt: linkEl.getAttribute('data-img-alt') || '', 
     }));
