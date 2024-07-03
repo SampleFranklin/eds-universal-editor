f63f1f70c06bb180a8e7e1006871b5abd56e17ef
prakharpathak <prakhar.pathak@nagarro.com>
2024-07-03 18:09:36 +0530

    added cardetails block

---
+++ blocks/car-detail-feature/car-detail-feature.js
@@ -0,0 +1,57 @@
+import ctaUtils from './ctaUtils.js';
+
+export default function decorate(block) {
+    const [
+          imageEl,
+          titleEl,
+          descriptionEl,
+          ctaTextEl,
+          ctaLinkEl,
+          ctaTargetEl,
+          featureTypeEl,
+    ] = block.children;
+
+    const image = imageEl?.querySelector('picture');
+    if (image) {
+        const img = image.querySelector('img');
+        img.removeAttribute('width');
+        img.removeAttribute('height');
+    }
+
+    const title = titleEl?.querySelector(':is(h1,h2,h3,h4,h5,h6)');
+    const description = Array.from(descriptionEl.querySelectorAll('p')).map((p) => p.outerHTML).join('');
+    const cta = ctaUtils.getLink(ctaLinkEl, ctaTextEl, ctaTargetEl, '');
+    const featureType = featureTypeEl?.textContent?.trim();
+
+    let ctaHtml = '';
+    if (cta) {
+      ctaHtml = `
+                     <div class="cta__actions">
+                       ${(cta) ? cta.outerHTML : ''}
+                     </div>
+                   `;
+    }
+
+    if(featureType) {
+      block.classList.add(featureType);
+    }
+
+    block.innerHTML = '';
+    block.insertAdjacentHTML(
+          'beforeend',
+          utility.sanitizeHtml(`
+                           <div class="feature__card">
+                               ${(image) ? `<div class="feature__image">${image.outerHTML}</div>` : ''}
+                               <div class="feature__content">
+                                   <div class="feature__info">
+                                       ${(title) ? `<div class="feature__title">${title.outerHTML}</div>` : ''}
+                                       ${(description) ? `<div class="feature__description">${description}</div>` : ''}
+                                   </div>
+                                   ${ctaHtml}
+                               </div>
+                           </div>
+                     `),
+        );
+
+
+}
