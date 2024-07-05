5770df1bb57fc83684c5a8efcb77a897463aa084
prakharpathak <prakhar.pathak@nagarro.com>
2024-07-05 13:40:15 +0530

    block js changes

--- blocks/section-navigator/section-navigator.js
+++ blocks/section-navigator/section-navigator.js
@@ -1,20 +1,25 @@
 export default function decorate(block) {
 
-    const newHtml = `
-            <nav>
-                <span class="dot" data-section="1"></span>
-                <span class="dot" data-section="2"></span>
-                <span class="dot" data-section="3"></span>
-                <span class="dot" data-section="4"></span>
-            </nav>
-    `;
+//    const newHtml = `
+//            <nav>
+//                <span class="dot" data-section="1"></span>
+//                <span class="dot" data-section="2"></span>
+//                <span class="dot" data-section="3"></span>
+//                <span class="dot" data-section="4"></span>
+//            </nav>
+//    `;
 
-    block.parentNode.parentNode.innerHTML = newHtml;
-    var navElement = document.querySelector('main .section-navigator-container nav');
-    var containerDiv = document.querySelector('.section-navigator-container');
-    var mainElement = document.querySelector('main');
-    if (navElement && containerDiv && mainElement) {
-        mainElement.insertBefore(navElement, mainElement.firstChild);
-        containerDiv.remove();
-    }
+//    block.innerHTML = newHtml;
+//    var navElement = document.querySelector('main .section-navigator-container nav');
+//    var containerDiv = document.querySelector('.section-navigator-container');
+//    var mainElement = document.querySelector('main');
+//    if (navElement && containerDiv && mainElement) {
+//        mainElement.insertBefore(navElement, mainElement.firstChild);
+//        containerDiv.remove();
+//    }
+
+    const sections = document.querySelectorAll('.section');
+    console.log(sections);
+
+
 }
