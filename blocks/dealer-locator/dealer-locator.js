85a6610331a22694f09a95f05e43f733edb5c772
yadalavineetha <yadala.vineetha@nagarro.com>
2024-07-06 14:44:32 +0530

    dealer locator changes

--- blocks/dealer-locator/dealer-locator.js
+++ blocks/dealer-locator/dealer-locator.js
@@ -41,7 +41,7 @@ export default function decorate(block) {
     // Create the HTML structure using template literals
     const dealerLocatorHtml = `
       <div class="dealer-locator__container">
-      <div class= "image"> style="image: url('${dealerLocator.image}');">
+      <div class= "image"> <img src="${dealerLocator.image}"/>
       </div>
         <p class="pre-title">${dealerLocator.pretitle}</p>
         <p class="description">${dealerLocator.description}</p>
