639c5fddcdc39aa711c1c0fc37a2a9ff7446b736
yadalavineetha <yadala.vineetha@nagarro.com>
2024-07-07 16:18:21 +0530

    dealer locator changes

--- blocks/dealer-locator/dealer-locator.js
+++ blocks/dealer-locator/dealer-locator.js
@@ -77,11 +77,4 @@ export default function decorate(block) {
       });
   }
 
-  // Example of adding a click event to CTAs
-  ctaElements.forEach(function(cta) {
-      cta.addEventListener('click', function(event) {
-          event.preventDefault();
-          alert('CTA clicked: ' + event.target.textContent);
-      });
-  });
 }
