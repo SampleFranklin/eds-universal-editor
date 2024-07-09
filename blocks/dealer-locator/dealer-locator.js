ccd5449ef8f0ae044e8a0adc29685b7304a1a87e
yadalavineetha <yadala.vineetha@nagarro.com>
2024-07-09 16:43:37 +0530

    Dealer locator Component

--- blocks/dealer-locator/dealer-locator.js
+++ blocks/dealer-locator/dealer-locator.js
@@ -1,10 +1,12 @@
+import utility from '../../utility/utility.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   function getDealerLocator() {
       const [
           imageEl,
           pretitleEl,
           descriptionEl,
-          ...ctaEls
+          ...ctasEl
       ] = block.children;
 
       const image = imageEl?.querySelector('picture img');
@@ -17,85 +19,67 @@ export default function decorate(block) {
           const altTextEl = imageEl?.querySelector('figcaption');
           altText = altTextEl?.textContent?.trim() || 'image';
       }
-
       const pretitle = pretitleEl?.textContent?.trim() || "";
       const description = Array.from(descriptionEl.querySelectorAll('p')).map((p) => p.outerHTML).join('');
+      const ctaElements = ctasEl.map((element, index) => {
+        const [ctaTextEl, linkEl] = element.children;
+        const ctaText = ctaTextEl?.textContent?.trim() || '';
+        const link = linkEl?.querySelector('.button-container a')?.href;
 
-      const ctas = ctaEls.map(ctaEl => {
-          const ctaTextEl = ctaEl.querySelector('p');
-          const ctaLinkEl = ctaEl.querySelector('a');
-          const ctaText = ctaTextEl?.textContent?.trim() || '';
-          const ctaLink = ctaLinkEl?.href || '#';
-          return { ctaText, ctaLink };
-      });
+        const newButton = document.createElement('a');
+        newButton.href = link;
+        newButton.innerHTML = `<p>${ctaText}</p>`;
 
-      return {
-          imgSrc,
-          altText,
-          pretitle,
-          description,
-          ctas
-      };
+        if (index === 0) {
+            newButton.classList.add('nav-button', 'active');
+        } else {
+            newButton.classList.add('nav-button');
+        }
+
+        element.innerHTML = '';
+        element.appendChild(newButton);
+
+        moveInstrumentation(element, newButton);
+
+        return element.innerHTML;
+    }).join('');
+    return {
+      image,
+      altText,
+      pretitle,
+      description,
+
+    }
   }
-
-  const dealerLocator = getDealerLocator();
-
-  // Create the HTML structure using template literals
-  const dealerLocatorHtml = `
-      <div class="dealer-locator__container">
-          <div class="section">
-              <div class="image">
-                  <img src="${dealerLocator.imgSrc}" alt="${dealerLocator.altText}">
-              </div>
-              <div class="overlay">
-                  <div class="dealer-locator__content">
-                      <p class="pre-title">${dealerLocator.pretitle}</p>
-                      <div class="description">${dealerLocator.description}</div>
-                  </div>
-                  <div class="dealer-locator__action">
-                      <ul>
-                          ${dealerLocator.ctas.map(cta => `
-                              <li class="cta-text">
-                                  <a href="${cta.ctaLink}">${cta.ctaText}</a>
-                              </li>
-                          `).join('')}
-                      </ul>
-                  </div>
-              </div>
-          </div>
-      </div>
-  `;
-
-  // Set the generated HTML to the block
-  block.innerHTML = dealerLocatorHtml;
+  
 
   
+
+  const dealerLocator = getDealerLocator();
+       const dealerLocatorHtml = utility.sanitizeHtml(`
+        ${(dealerLocator.image) ? dealerLocator.image.outerHTML : ''}
+         <div class="dealerLocator__content">
+           ${(dealerLocator.pretitle) ? `<p>${dealerLocator.pretitle}</p>` : ''}
+           
+           ${(dealerLocator.description) ? `${dealerLocator.description}` : ''}
+           <div class="buttons-container">
+            ${ctaElements}
+        </div>
+          </div>
+    `);
+          
+     
+  
+
+  
+  block.innerHTML = dealerLocatorHtml;
+
+  <div class="dealerLocator__wrapper right-seperator">
+            ${dealerLocatorHtml}
+            </div>
 }
 
 
 
-
   
-  // const imageElement = document.querySelector('.dealer-locator__container .image');
-  // imageElement.style.backgroundImage = `url(${dealerLocator.imageUrl})`;
-
-  // // Add scroll event listener for highlighting CTAs
-  // const ctaElements = document.querySelectorAll('.dealer-locator__container .dealer-locator__action .cta-text');
-  // if (ctaElements.length > 0) {
-  //     window.addEventListener('scroll', function() {
-  //         const scrollPosition = window.scrollY + window.innerHeight;
-
-  //         ctaElements.forEach(function(cta) {
-  //             const ctaPosition = cta.getBoundingClientRect().top + window.scrollY;
-
-  //             if (scrollPosition >= ctaPosition) {
-  //                 cta.classList.add('highlight');
-  //             } else {
-  //                 cta.classList.remove('highlight');
-  //             }
-  //         });
-  //     });
-  // }
-
-  
-
+ 
