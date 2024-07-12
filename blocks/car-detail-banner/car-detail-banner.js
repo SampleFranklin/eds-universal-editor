2e992a604c96a3c94cc85f850c84119729e81155
varunmaisuria <varun.maisuria@nagarro.com>
2024-07-12 18:21:48 +0530

    lint fix

--- blocks/car-detail-banner/car-detail-banner.js
+++ blocks/car-detail-banner/car-detail-banner.js
@@ -1,9 +1,9 @@
-import { fetchPlaceholders } from "../../scripts/aem.js";
-import ctaUtils from "../../utility/ctaUtils.js";
-import utility from "../../utility/utility.js";
+import { fetchPlaceholders } from '../../scripts/aem.js';
+import ctaUtils from '../../utility/ctaUtils.js';
+import utility from '../../utility/utility.js';
 
 export default async function decorate(block) {
-  let car = null;
+  let carObject = null;
   const { publishDomain } = await fetchPlaceholders();
   const [
     modelIdEl,
@@ -20,103 +20,87 @@ export default async function decorate(block) {
   ] = block.children;
 
   const requestOptions = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   };
 
   const modelId = modelIdEl?.textContent?.trim();
-  const graphQlEndpoint =
-    `${publishDomain}/graphql/execute.json/msil-platform/carDetailBanner;modelId=` +
-    modelId;
-  fetch(graphQlEndpoint, requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      car = result?.data?.carModelList?.items[0];
-      populateBanner(car);
-    })
-    .catch();
   function populateBanner(car) {
+    /* eslint no-underscore-dangle: 0 */
     const carImage = publishDomain + car.carImage._dynamicUrl;
-    const carLogoImage = publishDomain + car.carLogoImage._dynamicUrl;
+    const carLogoImage = car.carLogoImage._publishUrl;
     const startingPriceText = startingPriceTextEl?.textContent?.trim();
-    const testDriveText = Array.from(testDriveTextEl.querySelectorAll("p"))
+    const testDriveText = Array.from(testDriveTextEl.querySelectorAll('p'))
       .map((p) => p.outerHTML)
-      .join("");
+      .join('');
     const scrollMoreText = scrollMoreTextEl?.textContent?.trim();
     const primaryCta = ctaUtils.getLink(
       primaryCtaLinkEl,
       primaryCtaTextEl,
       primaryCtaTargetEl,
-      "button-secondary-dark"
+      'button-secondary-dark',
     );
     const secondaryCta = ctaUtils.getLink(
       secondaryCtaLinkEl,
       secondaryCtaTextEl,
       secondaryCtaTargetEl,
-      "button-secondary-light"
+      'button-secondary-light',
     );
 
-    const image = bgImageEl?.querySelector("picture");
-    const img = image.querySelector("img");
-    img.removeAttribute("width");
-    img.removeAttribute("height");
+    const image = bgImageEl?.querySelector('picture');
+    const img = image.querySelector('img');
+    img.removeAttribute('width');
+    img.removeAttribute('height');
 
-    let ctaHtml = "";
+    let ctaHtml = '';
     if (primaryCta || secondaryCta) {
       ctaHtml = `
                      <div class="banner__actions">
-                       ${primaryCta ? primaryCta.outerHTML : ""}
-                       ${secondaryCta ? secondaryCta.outerHTML : ""}
+                       ${primaryCta ? primaryCta.outerHTML : ''}
+                       ${secondaryCta ? secondaryCta.outerHTML : ''}
                      </div>
                    `;
     }
 
-    block.innerHTML = "";
+    block.innerHTML = '';
     block.insertAdjacentHTML(
-      "beforeend",
+      'beforeend',
       utility.sanitizeHtml(`
                    <div class="hero__banner">
-                       ${
-                         image
-                           ? `<div class="hero__banner--image">${image.outerHTML}</div>`
-                           : ""
-                       }
+                       ${image
+    ? `<div class="hero__banner--image">${image.outerHTML}</div>`
+    : ''
+}
                        <div class="banner__content">
                            <div class="banner__info container">
-                           ${
-                             car.carDescription
-                               ? `<div class="banner__carDescription">${car.carDescription}</div>`
-                               : ""
-                           }
-                               ${
-                                 carLogoImage && car.altText
-                                   ? `<div class="banner__carLogoImage"><img src="../../../icons/brezza-logo.svg" alt=${car.altText}></div>`
-                                   : ""
-                               }
-                               ${
-                                 startingPriceText
-                                   ? `<div class="banner__startingPriceText"><p>${startingPriceText}</p></div>`
-                                   : ""
-                               }
-                               ${
-                                 car.exShowroomPrice
-                                   ? `<div class="banner__exShowroomPrice">${car.exShowroomPrice}</div>`
-                                   : ""
-                               }
-                               ${
-                                 testDriveText
-                                   ? `<div class="banner__testDriveText">${testDriveText}</div>`
-                                   : ""
-                               }
+                           ${car.carDescription
+    ? `<div class="banner__carDescription">${car.carDescription}</div>`
+    : ''
+}
+                               ${carLogoImage && car.altText
+    ? `<div class="banner__carLogoImage"><img src=${carLogoImage} alt=${car.altText}></div>`
+    : ''
+}
+                               ${startingPriceText
+    ? `<div class="banner__startingPriceText"><p>${startingPriceText}</p></div>`
+    : ''
+}
+                               ${car.exShowroomPrice
+    ? `<div class="banner__exShowroomPrice">${car.exShowroomPrice}</div>`
+    : ''
+}
+                               ${testDriveText
+    ? `<div class="banner__testDriveText">${testDriveText}</div>`
+    : ''
+}
                                
                                
-                               ${
-                                 carImage && car.altText
-                                   ? `<div class="banner__carImage"><img src=${carImage} alt=${car.altText}></div>`
-                                   : ""
-                               }
+                               ${carImage && car.altText
+    ? `<div class="banner__carImage"><img src=${carImage} alt=${car.altText}></div>`
+    : ''
+}
                                
                                ${ctaHtml}
                            </div>
@@ -125,22 +109,22 @@ export default async function decorate(block) {
                        
                    </div>
                    <div class="hero__bottom-gradient">
-                    ${
-                      scrollMoreText
-                        ? `<div class="banner__scrollMoreText"><span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                    ${scrollMoreText
+    ? `<div class="banner__scrollMoreText"><span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
   <path d="M5.625 8.3135L5.625 2.25H6.375L6.375 8.3135L9.22312 5.46538L9.75 6L6 9.75L2.25 6L2.77688 5.46538L5.625 8.3135Z" fill="white"/>
 </svg></span>${scrollMoreText}</div>`
-                        : ""
-                    }
+    : ''
+}
                     </div>
-             `)
+             `),
     );
   }
-  // function extractBannerInfo(result){
-  //     const car = result?.data?.carModelList?.items[0];
-  //     const carImage = publishDomain+car.carImage._dynamicUrl;
-  //     const carLogoImage = publishDomain+car.carLogoImage._dynamicUrl;
-  //     const description = car.carDescription;
-  //     const carExShowroomPrice = car.exShowroomPrice;
-  //    }
+  const graphQlEndpoint = `${publishDomain}/graphql/execute.json/msil-platform/carDetailBanner;modelId=${modelId}`;
+  fetch(graphQlEndpoint, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      carObject = result?.data?.carModelList?.items[0];
+      populateBanner(carObject);
+    })
+    .catch();
 }
