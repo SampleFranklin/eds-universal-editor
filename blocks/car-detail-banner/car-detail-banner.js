0d186898cb3658e92f578a7d4f06d51074342f8b
Ashwani Kumar <ashwanikumar04@IN-FVFFLLELQ05D.local>
2024-07-12 17:13:58 +0530

    css and js changes

--- blocks/car-detail-banner/car-detail-banner.js
+++ blocks/car-detail-banner/car-detail-banner.js
@@ -49,7 +49,7 @@ export default async function decorate(block) {
       primaryCtaLinkEl,
       primaryCtaTextEl,
       primaryCtaTargetEl,
-      "button-primary-light"
+      "button-secondary-dark"
     );
     const secondaryCta = ctaUtils.getLink(
       secondaryCtaLinkEl,
@@ -127,7 +127,9 @@ export default async function decorate(block) {
                    <div class="hero__bottom-gradient">
                     ${
                       scrollMoreText
-                        ? `<div class="banner__scrollMoreText">${scrollMoreText}</div>`
+                        ? `<div class="banner__scrollMoreText"><span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+  <path d="M5.625 8.3135L5.625 2.25H6.375L6.375 8.3135L9.22312 5.46538L9.75 6L6 9.75L2.25 6L2.77688 5.46538L5.625 8.3135Z" fill="white"/>
+</svg></span>${scrollMoreText}</div>`
                         : ""
                     }
                     </div>
