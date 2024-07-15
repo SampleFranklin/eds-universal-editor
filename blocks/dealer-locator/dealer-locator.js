753ddaf9175e25340363360b93b1a30a78466514
yadalavineetha <yadala.vineetha@nagarro.com>
2024-07-15 16:12:37 +0530

    Dealer locator Component

--- blocks/dealer-locator/dealer-locator.js
+++ blocks/dealer-locator/dealer-locator.js
@@ -1,4 +1,5 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
+import utility from '../../utility/utility.js';
 
 export default function decorate(block) {
   function getDealerLocator() {
@@ -18,44 +19,45 @@ export default function decorate(block) {
       const alt = altTextEl?.textContent?.trim() || 'image';
       img.setAttribute('alt', alt);
       img.classList.add('image-container');
-      
     }
-    
-    const pretitle = pretitleEl?.textContent?.trim() || "";
+
+    const pretitle = pretitleEl?.textContent?.trim() || '';
     const description = Array.from(descriptionEl.querySelectorAll('p')).map((p) => p.outerHTML).join('');
-    const ctas = Array.from(ctaEls).map(ctaEl => {
+    const ctas = Array.from(ctaEls).map((ctaEl) => {
       const [ctaTextEl, ctaLinkEl] = ctaEl.children;
       const ctaText = ctaTextEl?.textContent?.trim() || '';
       const ctaLink = ctaLinkEl?.querySelector('a')?.href || '#';
-      return {
-        text: ctaText,
-        href: ctaLink,
-        ctaEl: ctaEl
-      };
+      const li = document.createElement('li');
+      li.innerHTML = `<a href="${ctaLink}" class="cta-text">${ctaText}</a>`;
+      moveInstrumentation(ctaEl, li);
+      return li.outerHTML;
     });
 
-    return { image, pretitle, description, ctas };
+    return {
+      image, pretitle, description, ctas,
+    };
   }
 
   const dealerLocator = getDealerLocator();
 
   // Create the links HTML structure
-  const ul = document.createElement('ul');
-  ul.classList.add('list-container');
-  dealerLocator.ctas.forEach(cta => {
-    const listItem = document.createElement('li');
-    const anchor = document.createElement('a');
-    anchor.href = cta.href;
-    anchor.textContent = cta.text;
-    anchor.classList.add('cta-text'); // Added class for CTA text
-   
-    listItem.appendChild(anchor);
-    moveInstrumentation(cta.ctaEl, listItem);
-    ul.appendChild(listItem);
-  });
+  // const ul = document.createElement('ul');
+  // ul.classList.add('list-container');
+  // dealerLocator.ctas.forEach(cta => {
+  //   const listItem = document.createElement('li');
+  //   const anchor = document.createElement('a');
+  //   anchor.href = cta.href;
+  //   anchor.textContent = cta.text;
+  //   anchor.classList.add('cta-text'); // Added class for CTA text
+  //   listItem.appendChild(anchor);
+  //   moveInstrumentation(cta.ctaEl, listItem);
+  //   ul.appendChild(listItem);
+  // });
 
-  const dealerLocatorHtml = `
-    <div class="dealer-locator__container">
+  block.innerHTML = utility.sanitizeHtml(`
+  
+    <div class="dealer-locator__container right-seperator">
+       <div class="dealer-locator__container">
       <div class="section">
         <div class="image-container">
           ${(dealerLocator.image) ? dealerLocator.image.outerHTML : ''}
@@ -66,19 +68,16 @@ export default function decorate(block) {
             <div class="dealer-locator__description">${dealerLocator.description}</div>
             <div class="dealer-locator__action">
               <div class="scroll-bar"></div>
-              ${ul.outerHTML}
+              <ul class="list-container">
+                ${dealerLocator.ctas.join('')}
+              </ul>
             </div>
           </div>
         </div>
       </div>
     </div>
-  `;
-
-  block.innerHTML = `
-    <div class="dealer-locator__container right-seperator">
-      ${dealerLocatorHtml}
     </div>
-  `;
+  `);
 
   function setupScrollHighlight() {
     const ctaElements = document.querySelectorAll('.dealer-locator__action .cta-text');
@@ -87,7 +86,7 @@ export default function decorate(block) {
     // Initial highlight setup
     function highlightFirstCTA() {
       ctaElements[0].classList.add('highlight');
-      //ctaElements[0].style.fontSize = '35px'; // Increase font size for highlighted CTA
+      // ctaElements[0].style.fontSize = '35px'; // Increase font size for highlighted CTA
 
       const ctaHeight = ctaElements[0].offsetHeight;
       const ctaOffsetTop = ctaElements[0].offsetTop;
@@ -99,14 +98,14 @@ export default function decorate(block) {
     highlightFirstCTA(); // Highlight the first CTA initially
 
     // Add hover effects
-    ctaElements.forEach((cta, index) => {
+    ctaElements.forEach((cta) => {
       cta.addEventListener('mouseover', () => {
-        ctaElements.forEach(cta => {
-          cta.classList.remove('highlight');
+        ctaElements.forEach((ctaElement) => {
+          ctaElement.classList.remove('highlight');
           // cta.style.fontSize = '24px'; // Reset font size for normal CTAs
         });
         cta.classList.add('highlight');
-        //cta.style.fontSize = '35px'; // Increase font size for highlighted CTA
+        // cta.style.fontSize = '35px'; // Increase font size for highlighted CTA
 
         const ctaHeight = cta.offsetHeight;
         const ctaOffsetTop = cta.offsetTop;
