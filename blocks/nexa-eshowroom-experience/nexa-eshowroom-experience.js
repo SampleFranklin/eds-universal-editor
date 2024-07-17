bd6d092098432ffb3567d1a290627a192caa8328
jaideepte <jaideep.teotia@nagarro.com>
2024-07-17 11:37:27 +0530

    js fix for eshowroom

--- blocks/nexa-eshowroom-experience/nexa-eshowroom-experience.js
+++ blocks/nexa-eshowroom-experience/nexa-eshowroom-experience.js
@@ -2,6 +2,7 @@ import {
   fetchPlaceholders,
 } from '../../scripts/aem.js';
 import carouselUtils from '../../utility/carouselUtils.js';
+import utility from '../../utility/utility.js';
 
 export default async function decorate(block) {
   const getFwdVideoHtml = (videoUrl) => {
@@ -30,9 +31,14 @@ export default async function decorate(block) {
     publishDomain,
     apiKey,
   } = await fetchPlaceholders();
-  const url = `${publishDomain}/content/nexa/services/token`;
-  const auth = await fetch(url);
-  const authorization = await auth.text();
+  const tokenUrl = `${publishDomain}/content/nexa/services/token`;
+  let authorization = null;
+  try {
+    const auth = await fetch(tokenUrl);
+    authorization = await auth.text();
+  } catch (e) {
+    authorization = '';
+  }
 
   const [componentIds, priceTextE1, secondaryBtnTextE1, secondaryBtnCtaE1] = block.children;
 
@@ -46,40 +52,110 @@ export default async function decorate(block) {
   const parentDiv = document.querySelector('.nexa-eshowroom-experience-container');
   parentDiv.setAttribute('id', componentId);
 
-  const graphQlEndpoint = `${publishDomain}/graphql/execute.json/msil-platform/CarModelListByPath`;
+  async function formatCurrency(value) {
+    const numericValue = String(value).replace(/[^\d.]/g, '');
+    const formatter = new Intl.NumberFormat('en-IN', {
+      style: 'currency',
+      currency: 'INR',
+      maximumFractionDigits: 0,
+    });
+    let formattedValue = formatter.format(numericValue);
 
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+    // Replace commas with spaces
+    formattedValue = formattedValue.replace(/,/g, ' ');
+    return formattedValue;
+  }
 
-  const response = await fetch(graphQlEndpoint, requestOptions);
-  const data = await response.json();
-  await carModelInfo(data);
+  async function getLocalStorage(key) {
+    return localStorage.getItem(key);
+  }
+
+  async function fetchPrice(modelCode, exShowRoomPrice) {
+    const storedPrices = await getLocalStorage('modelPrice') ? JSON.parse(await getLocalStorage('modelPrice')) : {};
+    if (storedPrices[modelCode] && storedPrices[modelCode].price[forCode]) {
+      return storedPrices[modelCode].price[forCode];
+    }
+    const channel = 'EXC';
+    const apiUrl = 'https://api.preprod.developersatmarutisuzuki.in/pricing/v2/common/pricing/ex-showroom-detail';
+
+    const params = {
+      forCode,
+      channel,
+    };
+
+    const headers = {
+      'x-api-key': apiKey,
+      Authorization: authorization,
+    };
+
+    const url = new URL(apiUrl);
+    Object.keys(params).forEach((key) => url.searchParams.append(key, params[key]));
+    let data;
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers,
+      });
+      data = await response.json();
+    } catch {
+      data = {};
+    }
+    if (data?.error === false && data?.data) {
+      const storedModelPrices = {};
+      const timestamp = new Date().getTime() + (1 * 24 * 60 * 60 * 1000); // 1 day from now
+      data.data.models.forEach((item) => {
+        const {
+          modelCd,
+        } = item;
+        const {
+          lowestExShowroomPrice,
+        } = item;
+
+        storedModelPrices[modelCd] = {
+          price: {
+            [forCode]: lowestExShowroomPrice,
+          },
+          timestamp,
+        };
+      });
+      localStorage.setItem('modelPrice', JSON.stringify(storedModelPrices));
+      return storedModelPrices[modelCode].price[forCode];
+    }
+    return exShowRoomPrice;
+  }
 
   async function carModelInfo(result) {
     const cars = result.data.carModelList.items;
 
     if (!Array.isArray(cars) || cars.length === 0) {
-      console.error('No car data found in the GraphQL response.');
       return;
     }
 
     const carItemsPromises = cars.map(async (itemEl, index) => {
       const car = cars[index];
-      const { carName } = car;
-      const { modelId } = car;
-      const { carDescription } = car;
+      const {
+        modelId,
+      } = car;
+      const {
+        carDescription,
+      } = car;
       const exShowRoomPrice = car.exShowroomPrice;
-      const { eshowroomDescription } = car;
-      const { eshowroomPrimaryCtaText } = car;
-      const carDetailsPagePath = car.carDetailsPagePath ? car.carDetailsPagePath._path : '#';
-      const eshowroomDesktopFwdVideo = car.eshowroomDesktopFwdVideo ? car.eshowroomDesktopFwdVideo._publishUrl : '';
-      const eshowroomDesktopReverseVideo = car.eshowroomDesktopReverseVideo ? car.eshowroomDesktopReverseVideo._publishUrl : '';
-      const eshowroomMobileFwdVideo = car.eshowroomMobileFwdVideo ? car.eshowroomMobileFwdVideo._publishUrl : '';
-      const eshowroomMobileReverseVideo = car.eshowroomMobileReverseVideo ? car.eshowroomMobileReverseVideo._publishUrl : '';
+      const {
+        eshowroomDescription,
+      } = car;
+      const {
+        eshowroomPrimaryCtaText,
+      } = car;
+      // eslint-disable-next-line
+            const carDetailsPagePath = car.carDetailsPagePath ? car.carDetailsPagePath._path : '#';
+      // eslint-disable-next-line
+            const eshowroomDesktopFwdVideo = car.eshowroomDesktopFwdVideo ? car.eshowroomDesktopFwdVideo._publishUrl : '';
+      // eslint-disable-next-line
+            const eshowroomDesktopReverseVideo = car.eshowroomDesktopReverseVideo ? car.eshowroomDesktopReverseVideo._publishUrl : '';
+      // eslint-disable-next-line
+            const eshowroomMobileFwdVideo = car.eshowroomMobileFwdVideo ? car.eshowroomMobileFwdVideo._publishUrl : '';
+      // eslint-disable-next-line
+            const eshowroomMobileReverseVideo = car.eshowroomMobileReverseVideo ? car.eshowroomMobileReverseVideo._publishUrl : '';
 
       let assetFwdHtml = '';
       let assetReverseHtml = '';
@@ -99,45 +175,46 @@ export default async function decorate(block) {
       const formattedCarPrice = await formatCurrency(carPrice);
 
       return `
-                <div>
-                    <div class="e-showroom__info-container">
-                        <div>
-                            <h2 class="text-heading-primary">${carDescription}</h2>
-                            <div class="e-showroom__price-text">${priceText}
-                                <span class="e-showroom__price">Rs ${formattedCarPrice}</span>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="e-showroom__subtitle">${eshowroomDescription}</div>
-                            <div class="e-showroom__action">
-                             <div class="cta cta__primary">
-                                <a target="_blank" href="${carDetailsPagePath}" title="${eshowroomPrimaryCtaText}" target="_self">${eshowroomPrimaryCtaText}</a>
-                                </div>
-                                 <div class="cta cta__secondary">
-                                <a target="_blank" href="${secondaryBtnCta}?${modelId}" title="${secondaryBtnText}" target="_self">${secondaryBtnText}</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="e-showroom__video-carousel">
-                        <div class="e-showroom__video-container">
-                            ${assetFwdHtml}
-                            ${assetReverseHtml}
-                        </div>
-                    </div>
-                </div>`;
+                  <div>
+                      <div class="e-showroom__info-container">
+                          <div>
+                              <h2 class="text-heading-primary">${carDescription}</h2>
+                              <div class="e-showroom__price-text">${priceText}
+                                  <span class="e-showroom__price">${formattedCarPrice}</span>
+                              </div>
+                          </div>
+                          <div>
+                              <div class="e-showroom__subtitle">${eshowroomDescription}</div>
+                              <div class="e-showroom__action">
+                               <div class="cta cta__primary">
+                                  <a target="_blank" href="${carDetailsPagePath}" title="${eshowroomPrimaryCtaText}" target="_self">${eshowroomPrimaryCtaText}</a>
+                                  </div>
+                                   <div class="cta cta__secondary">
+                                  <a target="_blank" href="${secondaryBtnCta}?${modelId}" title="${secondaryBtnText}" target="_self">${secondaryBtnText}</a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="e-showroom__video-carousel">
+                          <div class="e-showroom__video-container">
+                              ${assetFwdHtml}
+                              ${assetReverseHtml}
+                          </div>
+                      </div>
+                  </div>`;
     });
 
     const carItems = await Promise.all(carItemsPromises);
 
-    block.innerHTML = `
-            <div class="e-showroom__container">
-                <div class="e-showroom-carousel">
-                    <div class="e-showroom__slides">
-                        ${carItems.join('')}
-                    </div>
-                </div>
-            </div>`;
+    block.innerHTML = utility.sanitizeHtml(`
+              <div class="e-showroom__container">
+                  <div class="e-showroom-carousel">
+                      <div class="e-showroom__slides">
+                          ${carItems.join('')}
+                      </div>
+                  </div>
+              </div>
+              `);
 
     const controller = carouselUtils.init(
       block.querySelector('.e-showroom-carousel'),
@@ -150,8 +227,11 @@ export default async function decorate(block) {
           }
         },
         onNext: (currentSlide, targetSlide) => {
-          currentSlide.querySelector('.e-showroom__video')?.play();
-          return false;
+          if (targetSlide) {
+            currentSlide.querySelector('.e-showroom__video')?.play();
+            return false;
+          }
+          return true;
         },
         onPrev: (currentSlide, targetSlide) => {
           targetSlide
@@ -185,76 +265,23 @@ export default async function decorate(block) {
     });
   }
 
-  async function formatCurrency(value) {
-    let numericValue = String(value).replace(/[^\d.]/g, '');
-    numericValue = numericValue.split('.')[0];
-    let lastThree = numericValue.slice(-3);
-    const otherNumbers = numericValue.slice(0, -3);
-    if (otherNumbers !== '') {
-      lastThree = ` ${lastThree}`;
+  const graphQlEndpoint = `${publishDomain}/graphql/execute.json/msil-platform/CarModelListByPath`;
+
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const response = await fetch(graphQlEndpoint, requestOptions);
+    if (!response.ok) {
+      throw new Error(`GraphQL response was not ok: ${response.statusText}`);
     }
-    const formattedValue = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ' ') + lastThree;
-    return formattedValue;
-  }
-
-  async function getLocalStorage(key) {
-    return localStorage.getItem(key);
-  }
-
-  async function fetchPrice(modelCode, exShowRoomPrice) {
-    const storedPrices = await getLocalStorage('modelPrice') ? JSON.parse(await getLocalStorage('modelPrice')) : {};
-    if (storedPrices[modelCode] && storedPrices[modelCode].price[forCode]) {
-      return storedPrices[modelCode].price[forCode];
-    }
-    const channel = 'EXC';
-    const apiUrl = 'https://api.preprod.developersatmarutisuzuki.in/pricing/v2/common/pricing/ex-showroom-detail';
-
-    const params = {
-      forCode,
-      channel,
-    };
-
-    const headers = {
-      'x-api-key': apiKey,
-      Authorization: authorization,
-    };
-
-    const url = new URL(apiUrl);
-    Object.keys(params).forEach((key) => url.searchParams.append(key, params[key]));
-
-    const response = await fetch(url, {
-      method: 'GET',
-      headers,
-    });
     const data = await response.json();
-
-    try {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      if (data.error === false && data.data) {
-        const storedModelPrices = {};
-        const timestamp = new Date().getTime() + (1 * 24 * 60 * 60 * 1000); // 1 day from now
-        data.data.models.forEach((item) => {
-          const {
-            modelCd,
-          } = item;
-          const { lowestExShowroomPrice } = item;
-
-          storedModelPrices[modelCd] = {
-            price: {
-              [forCode]: lowestExShowroomPrice,
-            },
-            timestamp,
-          };
-        });
-        localStorage.setItem('modelPrice', JSON.stringify(storedModelPrices));
-        return storedModelPrices[modelCode].price[forCode];
-      }
-      return exShowRoomPrice;
-    } catch (error) {
-      console.error('Error fetching price:', error);
-      return exShowRoomPrice;
-    }
+    await carModelInfo(data);
+  } catch (e) {
+    throw new Error('GraphQL response was not ok');
   }
 }
