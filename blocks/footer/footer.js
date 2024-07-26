c0b703bfca1246d7f38f3e7bf9909bbc91ae1ad3
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-07-26 17:34:21 +0530

    Made middle Section More dynamic

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,6 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+import utility from '../../utility/utility.js';
 
 /**
  * Loads and decorates the footer
@@ -17,36 +18,111 @@ export default async function decorate(block) {
   block.textContent = '';
   const footer = document.createElement('div');
 
-  const topSection = fragment.firstElementChild;
   while (fragment.firstElementChild) {
     footer.append(fragment.firstElementChild);
   }
 
+  const topSection = footer.querySelector('.footer-top-section');
+
+  
+  // Getting Middle Section For now  
+  const middleSection = footer.querySelector('.footer-center-section');
+
+  function transformArray(arr) {
+    // 
+    let resultCol = [];
+    let defaultCol = [];
+
+    arr.forEach(value => {
+        if (value.classList.contains('link-column-wrapper')) {
+            if (defaultCol.length > 0 && !(defaultCol[0]?.classList?.contains('link-column-wrapper'))) {
+                resultCol.push(defaultCol);
+                defaultCol = [];
+            }
+            defaultCol.push(value);
+        } else {
+            if (defaultCol.length > 0 && defaultCol[0]?.classList.contains('link-column-wrapper')) {
+                resultCol.push(defaultCol);
+                defaultCol = [];
+            }
+            defaultCol.push(value);
+        }
+    });
+
+    if (defaultCol.length > 0) {
+        resultCol.push(defaultCol);
+    }
+
+    return resultCol;
+  }
+
+  function generateLinkGridHTML(linkColumns){
+    const linkColumnHTML = (linkColumns.length > 0) ? linkColumns.map((column) => column.outerHTML).join('') : '';
+    return linkColumnHTML || '';
+  }
+
+  function getHeadingFromDefaultCol(divElement) {
+    const headingSection = divElement?.querySelector('h1, h2, h3, h4, h5, h6');
+
+    if (headingSection) {
+      const parentEl= headingSection.parentElement;
+      parentEl.removeChild(headingSection);
+      headingSection.classList.add('contactUs__Heading');
+    }
+
+    return headingSection;
+  }
+
+  function processFinalColumns(finalColumns) {
+    let result = [];
+
+    finalColumns.forEach(columnGroup => {
+        if (columnGroup[0].classList.contains('link-column-wrapper')) {
+            // Create HTML for 'link-column-wrapper' divs
+            const linkColumnHTML = generateLinkGridHTML(columnGroup);
+            result.push(linkColumnHTML);
+        } else {
+            // Process elements that do not contain 'link-column-wrapper' divs
+            const columnHeading = getHeadingFromDefaultCol(columnGroup[0]);
+            const columnContent = columnGroup.map(column => column.outerHTML).join('');
+            result.push({ heading: columnHeading, content: columnContent });
+        }
+    });
+
+    return result;
+}
+
+  const middleSectionChildren = Array.from(middleSection.children);
+  const finalColumns = transformArray(middleSectionChildren);
+  const result = processFinalColumns(finalColumns);
+
+
   // const linkGridSection = footer.querySelector('.link-grid-wrapper');
-  const linkColumns = Array.from(footer.querySelectorAll('.link-grid-column'));
-  const linkCoulmnHTML= linkColumns.map(column => column.outerHTML).join('');
-  const contactUsSection = footer.querySelector(
-    '.footer-center-section .default-content-wrapper',
-  );
-  const socialLinks = footer.querySelector(
-    '.footer-center-section .contact-wrapper',
-  );
+  //const linkColumns = Array.from(footer.querySelectorAll('.link-grid-column'));
+  //const linkColumnHTML = (linkColumns.length > 0) ? linkColumns.map((column) => column.outerHTML).join('') : '';
+  // const contactUsSection = footer.querySelector(
+  //   '.footer-center-section .default-content-wrapper',
+  // );
+  // const socialLinks = footer.querySelector(
+  //   '.footer-center-section .contact-wrapper',
+  // );
   const bottomSection = footer.querySelector('.footer-bottom-section');
-  const bottomFirstSection = bottomSection.querySelectorAll(
+  const bottomFirstSection = bottomSection?.querySelectorAll(
     '.columns-wrapper > div > div > div',
   );
-  const bottomSecondSection = bottomSection.querySelector(
+  const bottomSecondSection = bottomSection?.querySelector(
     '.default-content-wrapper',
   );
-  const contactUsHeadingSection = contactUsSection.removeChild(
-    contactUsSection.querySelector('h3'),
-  );
 
-  contactUsHeadingSection.classList.add('contactUs__Heading');
-  contactUsSection.querySelector('p').classList.add('tollfree__element');
+  // const contactUsHeadingSection = contactUsSection?.querySelector('h3');
+  // if (contactUsHeadingSection) {
+  //   contactUsSection?.removeChild(contactUsHeadingSection);
+  //   contactUsHeadingSection.classList.add('contactUs__Heading');
+  // }
+
 
   const bottomSectionHtmlText = [];
-  bottomFirstSection.forEach((bottomElement) => {
+  bottomFirstSection?.forEach((bottomElement) => {
     const pElement = bottomElement.querySelectorAll('p');
     let pElementString = '';
     pElement.forEach((pStr) => {
@@ -55,8 +131,37 @@ export default async function decorate(block) {
     bottomSectionHtmlText.push(pElementString);
   });
 
-  let bottomSecondSectionHtml = '<div class="row">';
-  bottomSecondSection.querySelectorAll('p').forEach((bottomElement) => {
+  let bottomFirstSectionHtml = '';
+  const columnCount = 2;
+  if (bottomSectionHtmlText.length === columnCount) {
+    bottomFirstSectionHtml = `
+      <div class="row">
+        <div class="col-md-7">
+          <div class="footer__info-left">
+            ${bottomSectionHtmlText[0]}
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="footer__info-right">
+            ${bottomSectionHtmlText[1]}
+          </div>
+        </div>
+      </div>
+    `;
+  } else {
+    bottomFirstSectionHtml = `
+      <div class="row">
+        <div class="col-md-12">
+          <div class="footer__info-left">
+            ${(bottomFirstSectionHtml) ? bottomSectionHtmlText[0] : ''}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  let bottomSecondSectionHtml = '<div class="row centre-align">';
+  bottomSecondSection?.querySelectorAll('p')?.forEach((bottomElement) => {
     const anchor = bottomElement.querySelector('a');
     if (anchor) {
       bottomSecondSectionHtml += ` <li><a href=${anchor.href}>${anchor.textContent}</a></li>`;
@@ -77,8 +182,43 @@ export default async function decorate(block) {
     `;
   }
 
-  block.parentElement.classList.add('footer__arena');
-  block.innerHTML = `
+  // Define the external function for the click event
+  function accordionClick() {
+    const ulElement = this.parentElement.querySelector('ul');
+    if (ulElement.classList.contains('hide__section__mobile')) {
+      this.classList.add('expand');
+      this.classList.remove('collapse');
+      ulElement.classList.remove('hide__section__mobile');
+    } else {
+      this.classList.add('collapse');
+      this.classList.remove('expand');
+      ulElement.classList.add('hide__section__mobile');
+    }
+  }
+
+  const collapseSection = () => {
+    block.querySelector('.contactUs__section').classList.add('hide__section');
+    block
+      .querySelector('.link-container-section')
+      .querySelectorAll('ul')
+      .forEach((element) => {
+        element.classList.add('hide__section');
+      });
+  };
+
+  const expandSection = () => {
+    block
+      .querySelector('.contactUs__section')
+      .classList.remove('hide__section');
+    block
+      .querySelector('.link-container-section')
+      .querySelectorAll('ul')
+      .forEach((element) => {
+        element.classList.remove('hide__section');
+      });
+  };
+
+  block.innerHTML = utility.sanitizeHtml(`
     <div class="footer">
       <div class="container">
         ${topSectionHtml}
@@ -86,15 +226,15 @@ export default async function decorate(block) {
           <div class="col-md-9 footer__columns">
             <div class="link-grid-wrapper">
                 <div class="link-container-section">
-                  ${linkCoulmnHTML}
+                  ${result[0] || ''}
                 </div>  
-            </div>
+            </div>  
           </div>
           <div class="col-md-3 footer__columns footer__columns-contact">
-            ${contactUsHeadingSection.outerHTML}
+            ${(result[1].heading) ? result[1].heading.outerHTML : 'Contact Us'}
             <div class="contactUs__section hide__section__mobile">
-              ${contactUsSection.outerHTML}
-              ${socialLinks.innerHTML}
+              ${(result[1].content) ? result[1].content  : ''}
+             
             </div>
           </div>
           <div class="col-md-12">
@@ -102,25 +242,14 @@ export default async function decorate(block) {
           </div>
         </div>
         <!-- Footer info text begins -->
-        <div class="row">
-          <div class="col-md-6">
-            <div class="footer__info-left">
-              ${bottomSectionHtmlText[0]}
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="footer__info-right">
-              ${bottomSectionHtmlText[1]}
-            </div>
-          </div>
-        </div>
+        ${bottomFirstSectionHtml}
         <!-- Footer info text end -->
         <!-- Footer copyright begins -->
         ${bottomSecondSectionHtml}
         <!-- Footer copyright end -->
       </div>
     </div>
-  `;
+  `);
 
   const footerSeparatorElement = block.querySelector('.footer__separator');
   footerSeparatorElement.addEventListener(
@@ -145,39 +274,4 @@ export default async function decorate(block) {
     element.classList.add('collapse');
     element.addEventListener('click', accordionClick, false);
   });
-  // Define the external function for the click event
-  function accordionClick() {
-    const ulElement = this.parentElement.querySelector('ul');
-    if (ulElement.classList.contains('hide__section__mobile')) {
-      this.classList.add('expand');
-      this.classList.remove('collapse');
-      ulElement.classList.remove('hide__section__mobile');
-    } else {
-      this.classList.add('collapse');
-      this.classList.remove('expand');
-      ulElement.classList.add('hide__section__mobile');
-    }
-  }
-
-  const collapseSection = function () {
-    block.querySelector('.contactUs__section').classList.add('hide__section');
-    block
-      .querySelector('.link-container-section')
-      .querySelectorAll('ul')
-      .forEach((element) => {
-        element.classList.add('hide__section');
-      });
-  };
-
-  const expandSection = function () {
-    block
-      .querySelector('.contactUs__section')
-      .classList.remove('hide__section');
-    block
-      .querySelector('.link-container-section')
-      .querySelectorAll('ul')
-      .forEach((element) => {
-        element.classList.remove('hide__section');
-      });
-  };
 }
