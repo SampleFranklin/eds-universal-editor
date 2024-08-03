3029c47807a78ee052130b918f029121b2a6a90e
yadalavineetha <yadala.vineetha@nagarro.com>
2024-08-04 01:14:49 +0530

    Dealership Activities Component

--- blocks/dealership-activities/dealership-activities.js
+++ blocks/dealership-activities/dealership-activities.js
@@ -52,7 +52,18 @@ export default function decorate(block) {
       secondaryCta: "Directions",
       tab: "showroom_visit"
     },
-    // Add more stubbed data as needed
+    {
+      dealerName: "Mayuri Automobile Co. Ltd.",
+      image: "/content/dam/nexa-world/Ar_Vk_Maruti_Rangman_Front%203-4th%20Bridge%20Motion%20Shot_V3_SL%204.png",
+      description: "Upcoming test drive | Heads up! We have scheduled a test drive on 13th June for Wagon R",
+      scheduledTime: "14:30PM",
+      scheduledDate: "13th Jun, 2024",
+      contact: "9931242213",
+      emailId: "mandi@competent-maruti.com",
+      primaryCta: "Schedule a video call",
+      secondaryCta: "Directions",
+      tab: "showroom_visit"
+    },
   ];
 
   const dealership = getDealershipActivities();
