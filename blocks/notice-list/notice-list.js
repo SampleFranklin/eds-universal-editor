acf8284a2efe86542c70eba19e64dfdff7436e13
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-08-29 10:19:06 +0530

    Update notice-list.js

--- blocks/notice-list/notice-list.js
+++ blocks/notice-list/notice-list.js
@@ -7,26 +7,26 @@ export default function decorate(block) {
     const compTitle = titleEl.querySelector('h1, h2, h3, h4, h5, h6')|| "";
     
 
-    // const notices = noticesEl.map((notice) => {
-    //     const [listTitleEl, isNewEl, titleEl, descEl, cta1El, cta2El] = notice.children;
+    const notices = noticesEl.map((notice) => {
+        const [listTitleEl, isNewEl, titleEl, descEl, cta1El, cta2El] = notice.children;
 
-    //     return {
-    //         listTitle: listTitleEl?.textContent?.trim() || "",
-    //         isNew: isNewEl?.textContent?.trim() || "",
-    //         title: titleEl?.textContent?.trim() || "",
-    //         descEl: descEl?.innerHTML || "", 
-    //         cta1: cta1El?.textContent?.trim() || "",
-    //         cta2: cta2El?.textContent?.trim() || ""
-    //     };
-    // });
+        return {
+            listTitle: listTitleEl?.textContent?.trim() || "",
+            isNew: isNewEl?.textContent?.trim() || "",
+            title: titleEl?.textContent?.trim() || "",
+            descEl: descEl?.innerHTML || "", 
+            cta1: cta1El?.textContent?.trim() || "",
+            cta2: cta2El?.textContent?.trim() || ""
+        };
+    });
 
    
 
-    // const listOfNotice = notices.map((notice, index) => `
-    // <p>
-    //     <a href="#" data-index="${index}">${notice.listTitle} ${notice.isNew}</a>
-    // </p>
-    // `).join('');
+    const listOfNotice = notices.map((notice, index) => `
+    <p>
+        <a href="#" data-index="${index}">${notice.listTitle} ${notice.isNew}</a>
+    </p>
+    `).join('');
 
  
    
