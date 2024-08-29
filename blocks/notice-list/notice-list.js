e42ef673a521ed14da0b147bea65d1a3366443b2
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-08-29 10:15:29 +0530

    Update notice-list.js

--- blocks/notice-list/notice-list.js
+++ blocks/notice-list/notice-list.js
@@ -7,16 +7,36 @@ export default function decorate(block) {
     const compTitle = titleEl.querySelector('h1, h2, h3, h4, h5, h6')|| "";
     
 
-    
+    const notices = noticesEl.map((notice) => {
+        const [listTitleEl, isNewEl, titleEl, descEl, cta1El, cta2El] = notice.children;
+
+        return {
+            listTitle: listTitleEl?.textContent?.trim() || "",
+            isNew: isNewEl?.textContent?.trim() || "",
+            title: titleEl?.textContent?.trim() || "",
+            descEl: descEl?.innerHTML || "", 
+            cta1: cta1El?.textContent?.trim() || "",
+            cta2: cta2El?.textContent?.trim() || ""
+        };
+    });
 
    
 
-   console.log(listOfNotice);
+    // const listOfNotice = notices.map((notice, index) => `
+    // <p>
+    //     <a href="#" data-index="${index}">${notice.listTitle} ${notice.isNew}</a>
+    // </p>
+    // `).join('');
+
+ 
    
     block.innerHTML = `
         <div id="container1" class="container">
             <p>${componentSeparator}</p>
             ${compTitle.outerHTML}
+            <div>
+            <p>Sample text</p>
+            </div>
         </div>`
     
     return block;
