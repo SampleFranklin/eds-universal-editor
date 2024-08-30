3613cb4d775243c2ba2e4da329b225d39eb1bcff
VISHAL DESWAL <vishaldeswal2000@gmail.com>
2024-08-30 14:17:49 +0530

    Es Lint issues resolved

--- blocks/notice-list/notice-list.js
+++ blocks/notice-list/notice-list.js
@@ -1,184 +1,160 @@
 import vinCheckDecorate from '../vin-check/vin-check.js';
+
 export default async function decorate(block) {
+  function keepOnlyFirstDivInNotices(noticesElClone) {
+    noticesElClone.forEach((notice, index) => {
+      const divs = notice.querySelectorAll('div');
 
+      divs.forEach((div, divIndex) => {
+        if (divIndex > 0) {
+          div.remove();
+        } else if (divIndex === 0) {
+          const pElement = div.querySelector('p');
+          if (pElement) {
+            pElement.classList.add('notice_list-title');
 
-    function keepOnlyFirstDivInNotices(noticesElClone) {
-        // Iterate over each element in noticesElClone
-        noticesElClone.forEach((notice, index) => {
-            // Find all div elements within the current notice
-            const divs = notice.querySelectorAll('div');
+            const secondDiv = notice.querySelectorAll('div')[1];
+            const secondDivText = (secondDiv?.textContent) ? `<span>${secondDiv?.textContent.trim()}</span>` : '';
 
-            // Iterate over the divs, keep the first one, and remove the rest
-            divs.forEach((div, divIndex) => {
-                if (divIndex > 0) {
-                    div.remove();
-                } else if (divIndex === 0) {
-                    const pElement = div.querySelector('p');
-                    if (pElement) {
-                        pElement.classList.add('notice_list-title');
+            const aElement = document.createElement('a');
+            aElement.href = '#';
+            aElement.innerHTML = `${pElement.textContent || ''} ${secondDivText} `;
+            aElement.setAttribute('data-index', index);
 
-                        const secondDiv = notice.querySelectorAll('div')[1];
-                        const secondDivText = (secondDiv?.textContent) ? `<span>${secondDiv?.textContent.trim()}</span>` : '';
+            pElement.innerHTML = '';
+            pElement.append(aElement);
+          }
+        }
+      });
+    });
 
-                        const aElement = document.createElement('a');
-                        aElement.href = '#';
-                        aElement.innerHTML = `${pElement.textContent || ''} ${secondDivText} `;
-                        aElement.setAttribute('data-index', index);
+    return noticesElClone;
+  }
 
-                        pElement.innerHTML = "";
-                        pElement.append(aElement);
+  function emptyContainer3() {
+    const container3 = block.querySelector('#container3');
+    container3.innerHTML = '';
+    return container3;
+  }
 
-                    }
-                }
-            });
-        });
+  function showcontainer2() {
+    const { parentElement } = block.parentElement;
+    const siblings = Array.from(parentElement.children)
+      .filter((child) => child !== block.parentElement);
+    siblings.forEach((siblingBlock) => siblingBlock.classList.remove('hidden'));
 
-        return noticesElClone;
-    }
+    block.querySelector('#container1').classList.add('hidden');
+    block.querySelector('#container3').classList.add('hidden');
+    block.querySelector('#container2').classList.remove('hidden');
+  }
 
-    function emptyContainer3(){
-        const container3= block.querySelector('#container3')
-        container3.innerHTML='';
+  function showcontainer1(submitLink) {
+    const submitBtn = block.querySelector('#container1 .check');
+    submitBtn.setAttribute('submit-link', submitLink);
 
-        return container3;
-    }
-    
-    function showDescription(index) {
+    block.querySelector('#container3').classList.add('hidden');
+    block.querySelector('#container1').classList.remove('hidden');
+  }
 
-        const parentElement = block.parentElement.parentElement;
-        const siblings = Array.from(parentElement.children).filter(child => child !== block.parentElement);
-        siblings.forEach(block => block.classList.add('hidden'));
+  const [comptitleEl, vinCheckEL, ...noticesEl] = block.children;
 
-        block.querySelector('#container2').classList.add('hidden');
+  const compSeparator = comptitleEl?.querySelector('p') || '';
+  compSeparator.classList.add('comp-separator');
+
+  const componentHeading = comptitleEl?.querySelector(':is(h1,h2,h3,h4,h5,h6)') || '';
+  componentHeading.classList.add('comp-heading');
+
+  const noticesElClone = Array.from(noticesEl).map((notice) => {
+    const noticeClone = notice.cloneNode(true);
+    return noticeClone;
+  });
+  const alteredNoticesEL = keepOnlyFirstDivInNotices(noticesElClone);
+  const listTiltleHTML = alteredNoticesEL.map((notice) => notice.outerHTML).join('');
+
+  const noticesDetails = noticesEl.map((notice) => {
+    const [listTitleEl,
+      isNewEl,
+      titleEl,
+      descEl,
+      cta1El,
+      cta2El] = notice.children;
+    return {
+      listTitle: listTitleEl?.textContent?.trim() || '',
+      isNew: isNewEl?.textContent?.trim() || '',
+      title: titleEl?.textContent?.trim() || '',
+      descEl: descEl?.innerHTML || '',
+      cta1: cta1El?.textContent?.trim() || '',
+      cta2: cta2El?.textContent?.trim() || '',
+    };
+  });
+
+  await vinCheckDecorate(vinCheckEL);
+
+  block.innerHTML = `
+        <div id="container1" class="container hidden">
+            <button  id="backButton2">Back</button>    
+        </div>
+
+        <div id="container2" class="container">
+            <div class="notice-list__heading_container">
+                ${compSeparator?.outerHTML || ''}
+                ${componentHeading?.outerHTML || ''}
+            </div>
+            
+            <div class="notice-list__list_container">
+                ${listTiltleHTML}  
+            </div>
+        </div>
         
+        <div id="container3" class="container hidden">
+        </div>`;
 
-        const container3 = emptyContainer3();
-       
-        const notice = noticesDetails[index];
+  function showDescription(index) {
+    const { parentElement } = block.parentElement;
+    const siblings = Array.from(parentElement.children)
+      .filter((child) => child !== block.parentElement);
+    siblings.forEach((siblingBlock) => siblingBlock.classList.add('hidden'));
 
-        const noticeHTML = `
+    block.querySelector('#container2').classList.add('hidden');
+
+    const notice = noticesDetails[index];
+    const noticeHTML = `
             <div id="description${index}" class="description">
                 <button id="backButton">${notice.cta1}</button>
                 <h4>${notice.title}</h4>
                 ${notice.descEl}
                 <button id="checkHereBtn" data-submit-link="${index}">${notice.cta2}</button>
-            </div>
-            `;
+            </div>`;
 
-      container3.innerHTML = noticeHTML;
-     
-      container3.querySelector("#backButton").addEventListener("click", ()=>{
-         emptyContainer3();
-         showcontainer2();
-     });
-
-     container3.querySelector("#checkHereBtn").addEventListener("click", function (event) {
-         event.preventDefault();
-         const index = this.getAttribute("data-submit-link");
-         emptyContainer3();
-         showcontainer1(index);
-     });
-    
-     container3.classList.remove('hidden');
-
-    }
-
-    function showcontainer2() {
-        const parentElement = block.parentElement.parentElement;
-        const siblings = Array.from(parentElement.children).filter(child => child !== block.parentElement);
-        siblings.forEach(block => block.classList.remove('hidden'));
-
-        block.querySelector('#container1').classList.add('hidden');
-        block.querySelector('#container3').classList.add('hidden');
-        block.querySelector('#container2').classList.remove('hidden');
-    }
-
-   function showcontainer1(submitLink) {
-        const submitBtn = block.querySelector("#container1 .check");
-        submitBtn.setAttribute('submit-link', submitLink);
-
-        block.querySelector('#container3').classList.add('hidden');
-        block.querySelector('#container1').classList.remove('hidden');
-    }
-
-
-    const blockClone = block.cloneNode(true);
-    console.log(blockClone)
-
-    const [titleEl, vinCheckEL, ...noticesEl] = block.children;
-
-    const compSeparator = titleEl?.querySelector('p') || "";
-    compSeparator.classList.add('comp-separator');
-    const componentHeading = titleEl?.querySelector(':is(h1,h2,h3,h4,h5,h6)') || "";
-    componentHeading.classList.add('comp-heading');
-
-    const noticesElClone = Array.from(noticesEl).map((notice) => {
-        let noticeClone = notice.cloneNode(true);
-        return noticeClone;
+    const container3 = emptyContainer3();
+    container3.innerHTML = noticeHTML;
+    container3.querySelector('#backButton').addEventListener('click', () => {
+      emptyContainer3();
+      showcontainer2();
     });
-
-    const noticesDetails = noticesEl.map(notice => {
-        const [listTitleEl, isNewEl, titleEl, descEl, cta1El, cta2El] = notice.children;
-
-        return {
-            listTitle: listTitleEl?.textContent?.trim() || "",
-            isNew: isNewEl?.textContent?.trim() || "",
-            title: titleEl?.textContent?.trim() || "",
-            descEl: descEl?.innerHTML || "",
-            cta1: cta1El?.textContent?.trim() || "",
-            cta2: cta2El?.textContent?.trim() || ""
-        };
+    container3.querySelector('#checkHereBtn').addEventListener('click', (event) => {
+      event.preventDefault();
+      const subLink = event.currentTarget.getAttribute('data-submit-link');
+      emptyContainer3();
+      showcontainer1(subLink);
     });
+    container3.classList.remove('hidden');
+  }
 
+  const links = block.querySelectorAll('#container2 a');
 
-    let alteredNoticesEL = keepOnlyFirstDivInNotices(noticesElClone);
-    const listTiltleHTML = alteredNoticesEL.map(notice => {
-        return notice.outerHTML
-    }).join('');
-
-    await vinCheckDecorate(vinCheckEL);
-
-        block.innerHTML = `
-             <div id="container1" class="container hidden">
-                <button  id="backButton2">Back</button>    
-                
-            </div>
-
-             <div id="container2" class="container">
-                <div class="notice-list__heading_container">
-                    ${compSeparator?.outerHTML || ''}
-                    ${componentHeading?.outerHTML || ''}
-                </div>
-                <div class="notice-list__list_container">
-                    ${listTiltleHTML}  
-                </div>
-                
-            </div>
-
-            <div id="container3" class="container hidden">
-            </div>
-
-           
-    `;
-
-
-
-    //Adding Event Listener to list titles
-    const links = block.querySelectorAll("#container2 a");
-    links.forEach(link => {
-        link.addEventListener("click", function (event) {
-            event.preventDefault();
-            const index = this.getAttribute("data-index");
-            showDescription(index);
-        });
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const index = event.currentTarget.getAttribute('data-index');
+      showDescription(index);
     });
+  });
 
-    
-    block.querySelectorAll("#backButton2").forEach(btn => {
-        btn.addEventListener("click", showcontainer2);
-    })
+  block.querySelectorAll('#backButton2').forEach((btn) => {
+    btn.addEventListener('click', showcontainer2);
+  });
 
-    const container1 = block.querySelector("#container1");
-    container1.append(vinCheckEL)
-
+  const container1 = block.querySelector('#container1');
+  container1.append(vinCheckEL);
 }
