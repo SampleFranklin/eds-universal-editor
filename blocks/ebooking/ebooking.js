a14394d324e76a9f8db9a630f2c53c8fe44cb791
sumityadav <sumit.yadav@nagarro.com>
2024-09-05 18:18:07 +0530

    Ebook POC JS

--- blocks/ebooking/ebooking.js
+++ blocks/ebooking/ebooking.js
@@ -24,53 +24,30 @@ export default function decorate(block) {
     });
   }
 
-  // function to fetch refreshed access token
-//  async function fetchData() {
-//    const endpoint = 'https://api.preprod.developersatmarutisuzuki.in/auth/consumers/gettoken';
-//    const body = {
-//      clientId: '2cq5is6643ojh60sej6na0030g',
-//      clientSecret: '89hrprllq8ivjp1tj983vclkncr4proudr7uvb3uiv1q88nspbm',
-//    };
-//    try {
-//      const response = await fetch(
-//        endpoint,
-//        {
-//          method: 'POST',
-//          headers: {
-//            'Content-Type': 'application/json',
-//          },
-//          body: JSON.stringify(body),
-//        },
-//      );
-//      if (!response.ok) {
-//        throw new Error('Network response was not ok');
-//      }
-//      // Store the access token to localstorage
-//      const data = await response.json();
-//      localStorage.setItem('apimToken', data);
-//    } catch (error) {
-//      throw new Error('Error Fetching data');
-//    }
-//  }
-
-//  const refreshTokenInterval = 60 * 60 * 1000; // 1 hour
-//  fetchData();
-//  setInterval(fetchData, refreshTokenInterval);
 
   loadScript('https://d1lzjb5dkpjvo6.cloudfront.net/martech-ebook.js');
   loadStylesheet('https://d1lzjb5dkpjvo6.cloudfront.net/styles.css');
-  // Create the div element
+
   let div = document.createElement("div");
-
   let martechEbook = document.createElement("martech-ebook");
-
   martechEbook.setAttribute("channel", "NRM");
 
   div.appendChild(martechEbook);
 
-
- console.log("Ebook Component Loaded");
-
+  console.log("Ebook Component Loaded");
   block.innerHTML = '';
   block.appendChild(div);
+
+  const component = document.querySelector("martech-ebook");
+  component.inpCarDetail = '{"modelCode": "VZ","variantCode": "VZRCCV1","colorCode": "Z7Q"}';
+  component.inpSourceDetail = '{"sourceName":"Hyperlocal-Arena","sourceUrl": "https://scstage-arena-cd.azurewebsites.net/e-booking/microsite" }';
+
+  //Listen To Events
+  const myElement = document.querySelector("martech-ebook");
+      myElement?.addEventListener("martech-ebook-ga-event", (event) => {
+        const eventData = event.detail.data;
+        const tableName = event.detail.tableName;
+        const type = event.detail.type; // values: event-data, form-data;
+        // Call GA track event
+      });
 }
